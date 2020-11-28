5a6233af1289b04e3d651c2456d0b17e0d66057d
Juho Vepsäläinen <bebraw@gmail.com>
2020-11-28 20:44:09 +0100

    chore: Serve html

--- mod.ts
+++ mod.ts
@@ -6,11 +6,45 @@ async function serve(port: number) {
   console.log(`Serving at ${port}`);
 
   app.use((context) => {
-    // TODO
-    context.response.body = "hello";
+    context.response.headers.set("Content-Type", "text/html; charset=UTF-8");
+    context.response.body = new TextEncoder().encode(
+      htmlTemplate({ title: "Gustwind", body: "Hello world" }),
+    );
   });
 
   await app.listen({ port });
+}
+
+type Meta = Record<string, string>;
+
+function htmlTemplate(
+  { title, meta, head, body }: {
+    title: string;
+    meta?: Meta;
+    styleTag?: string;
+    head?: string;
+    body?: string;
+  },
+) {
+  return `<html>
+  <head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>${title || ""}</title>
+    ${generateMeta(meta)}
+    ${head || ""}
+  </head>
+  <body>${body || ""}</body>
+</html>`;
+}
+
+function generateMeta(meta?: Meta) {
+  if (!meta) {
+    return "";
+  }
+
+  return Object.entries(meta).map(([key, value]) =>
+    `<meta name="${key}" content="${value}"></meta>`
+  ).join("\n");
 }
 
 // TODO: Make this configurable
