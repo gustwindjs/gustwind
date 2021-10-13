01bd6c6505aaecf2db0195e75a98aa27cd25fbca
Juho Vepsäläinen <bebraw@gmail.com>
2021-10-13 16:11:58 +0300

    fix: Improve mobile layout

--- transforms/markdown.ts
+++ transforms/markdown.ts
@@ -12,7 +12,6 @@ hljs.registerLanguage("typescript", typescript);
 hljs.registerLanguage("ts", typescript);
 
 marked.setOptions({
-  renderer: new marked.Renderer(),
   gfm: true,
   breaks: false,
   pedantic: false,
@@ -21,6 +20,36 @@ marked.setOptions({
   smartypants: true,
   highlight: (code, language) => {
     return hljs.highlight(code, { language }).value;
+  },
+});
+
+marked.use({
+  renderer: {
+    code(code, infostring, escaped) {
+      const lang = (infostring || "").match(/\S*/)[0];
+      if (this.options.highlight) {
+        const out = this.options.highlight(code, lang);
+        if (out != null && out !== code) {
+          escaped = true;
+          code = out;
+        }
+      }
+
+      code = code.replace(/\n$/, "") + "\n";
+
+      if (!lang) {
+        return "<pre><code>" +
+          code +
+          "</code></pre>\n";
+      }
+
+      return '<pre class="overflow-auto"><code class="' +
+        this.options.langPrefix +
+        lang +
+        '">' +
+        code +
+        "</code></pre>\n";
+    },
   },
 });
 
