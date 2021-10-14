2bab81b090c6cb16ca1d8678e981bc7e6162502d
Juho Vepsäläinen <bebraw@gmail.com>
2021-10-14 10:29:19 +0300

    fix: Disable unknown directive warnings

--- src/sharedTwindSetup.ts
+++ src/sharedTwindSetup.ts
@@ -1,7 +1,10 @@
 import * as colors from "twind-colors";
 import typography from "twind-typography";
 
+// TODO: A good production optimization would be to enable hash: true
 const sharedTwindSetup = {
+  // https://twind.dev/handbook/configuration.html#mode
+  mode: "silent",
   theme: { colors },
   plugins: {
     // TODO: How to override blockquote styles?
