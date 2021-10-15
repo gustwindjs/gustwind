110474217e85e910706f4b46ace67a662a3cb682
Juho Vepsäläinen <bebraw@gmail.com>
2021-10-15 19:53:36 +0300

    chore: Sketch out editor loading

--- pages/playground.ts
+++ pages/playground.ts
@@ -1,33 +1,11 @@
 /// <reference lib="dom" />
-import "sidewind";
-import { setup } from "twind-shim";
-import { tw } from "twind";
-import JSONEditor from "jsoneditor";
-import sharedTwindSetup from "../src/sharedTwindSetup.ts";
-import updateMeta from "../src/updateMeta.ts";
-import { renderBody } from "../src/renderBody.ts";
-import type { Page } from "../types.ts";
 
-console.log("Hello from the playground");
-
-setup({
-  target: document.body,
-  ...sharedTwindSetup("development"),
-});
-
+// TODO: Share this with the page editor in a nice way
 const pageEditorId = "pageEditor";
 
-function createPlaygroundEditor() {
-  const stylesheet = document.createElement("link");
-  stylesheet.setAttribute("rel", "stylesheet");
-  stylesheet.setAttribute("type", "text/css");
-  stylesheet.setAttribute(
-    "href",
-    "https://cdn.jsdelivr.net/gh/josdejong/jsoneditor/dist/jsoneditor.min.css",
-  );
+let tries = 0;
 
-  document.body.appendChild(stylesheet);
-
+async function toggleEditor() {
   const mainElement = document.querySelector("main");
 
   if (!mainElement) {
@@ -36,47 +14,8 @@ function createPlaygroundEditor() {
     return;
   }
 
-  const pageEditorElement = document.createElement("div");
-  pageEditorElement.setAttribute("id", pageEditorId);
-  pageEditorElement.setAttribute(
-    "class",
-    tw`fixed bg-white bottom-0 w-full max-h-1/2`,
-  );
-  pageEditorElement.style.visibility = "hidden";
-
-  mainElement.parentNode?.insertBefore(
-    pageEditorElement,
-    mainElement.nextSibling,
-  );
-
-  const editor = new JSONEditor(pageEditorElement, {
-    onChangeJSON: async (pageJson: Page) => {
-      updateMeta(pageJson.meta);
-
-      const bodyMarkup = await renderBody(
-        pageJson,
-        pageJson.page,
-        {}, // TODO: Components should go here - load through data as well
-        {}, // Data context is empty for now
-        "/playground/", // hard coded for now
-      );
-
-      mainElement.innerHTML = bodyMarkup;
-    },
-  });
-
-  fetch("./definition.json").then((res) => res.json()).then((d) =>
-    editor.set(d)
-  );
-}
-
-createPlaygroundEditor();
-
-function toggleEditor() {
-  const mainElement = document.querySelector("main");
-
-  if (!mainElement) {
-    console.error("Failed to find body element");
+  if (tries > 5) {
+    console.error("Failed to load page editor script");
 
     return;
   }
@@ -90,8 +29,15 @@ function toggleEditor() {
   if (!pageEditorElement) {
     console.error("Failed to find page editor element");
 
-    // TODO: This should trigger addScript against the editor script to
-    // load it async as it's too heavy
+    try {
+      await addScript("pageEditor.js");
+
+      toggleEditor();
+
+      tries++;
+    } catch (err) {
+      console.error(err);
+    }
 
     return;
   }
@@ -103,10 +49,16 @@ function toggleEditor() {
   }
 }
 
-function addScript(src) {
-  var s = document.createElement("script");
-  s.setAttribute("src", src);
-  document.body.appendChild(s);
+function addScript(src: string): Promise<void> {
+  return new Promise((resolve, reject) => {
+    const script = document.createElement("script");
+
+    script.setAttribute("src", src);
+    script.onload = () => resolve();
+    script.onerror = () => reject();
+
+    document.body.appendChild(script);
+  });
 }
 
 declare global {
---
+++ src/pageEditor.ts
@@ -0,0 +1,73 @@
+/// <reference lib="dom" />
+import "sidewind";
+import { setup } from "twind-shim";
+import { tw } from "twind";
+import JSONEditor from "jsoneditor";
+import sharedTwindSetup from "../src/sharedTwindSetup.ts";
+import updateMeta from "../src/updateMeta.ts";
+import { renderBody } from "../src/renderBody.ts";
+import type { Page } from "../types.ts";
+
+console.log("Hello from the page editor");
+
+setup({
+  target: document.body,
+  ...sharedTwindSetup("development"),
+});
+
+const pageEditorId = "pageEditor";
+
+function createPlaygroundEditor() {
+  const stylesheet = document.createElement("link");
+  stylesheet.setAttribute("rel", "stylesheet");
+  stylesheet.setAttribute("type", "text/css");
+  stylesheet.setAttribute(
+    "href",
+    "https://cdn.jsdelivr.net/gh/josdejong/jsoneditor/dist/jsoneditor.min.css",
+  );
+
+  document.body.appendChild(stylesheet);
+
+  const mainElement = document.querySelector("main");
+
+  if (!mainElement) {
+    console.error("Failed to find body element");
+
+    return;
+  }
+
+  const pageEditorElement = document.createElement("div");
+  pageEditorElement.setAttribute("id", pageEditorId);
+  pageEditorElement.setAttribute(
+    "class",
+    tw`fixed bg-white bottom-0 w-full max-h-1/2`,
+  );
+  pageEditorElement.style.visibility = "hidden";
+
+  mainElement.parentNode?.insertBefore(
+    pageEditorElement,
+    mainElement.nextSibling,
+  );
+
+  const editor = new JSONEditor(pageEditorElement, {
+    onChangeJSON: async (pageJson: Page) => {
+      updateMeta(pageJson.meta);
+
+      const bodyMarkup = await renderBody(
+        pageJson,
+        pageJson.page,
+        {}, // TODO: Components should go here - load through data as well
+        {}, // Data context is empty for now
+        "/playground/", // hard coded for now
+      );
+
+      mainElement.innerHTML = bodyMarkup;
+    },
+  });
+
+  fetch("./definition.json").then((res) => res.json()).then((d) =>
+    editor.set(d)
+  );
+}
+
+createPlaygroundEditor();
