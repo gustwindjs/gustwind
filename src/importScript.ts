6606021e1dfc8a425b8459ba0bce28f9cb29e927
Juho Vepsäläinen <bebraw@gmail.com>
2021-10-16 18:58:29 +0300

    fix: Set module type correctly and generate script tag only once

--- src/importScript.ts
+++ src/importScript.ts
@@ -1,7 +1,16 @@
+const loadedScripts: Record<string, boolean> = {};
+
 function importScript(src: string): Promise<void> {
   return new Promise((resolve, reject) => {
+    if (loadedScripts[src]) {
+      return;
+    }
+
+    loadedScripts[src] = true;
+
     const script = document.createElement("script");
 
+    script.setAttribute("type", "module");
     script.setAttribute("src", src);
     script.onload = () => resolve();
     script.onerror = () => reject();
