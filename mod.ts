4cabbaeccbbde5db30ec585439c5c68109f53433
Juho Vepsäläinen <bebraw@gmail.com>
2021-11-03 15:28:14 +0200

    chore: Expose transforms

--- mod.ts
+++ mod.ts
@@ -1,4 +1,5 @@
 import { build } from "./src/build.ts";
 import { serve } from "./src/serve.ts";
+import * as transforms from "./transforms/index.ts";
 
-export { build, serve };
+export { build, serve, transforms };
---
+++ transforms/index.ts
@@ -0,0 +1,4 @@
+import markdown from "./markdown.ts";
+import reverse from "./reverse.ts";
+
+export { markdown, reverse };
