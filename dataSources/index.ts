d9ef8eef5b612f9d7dddcb41355cd0b8af3f0928
Juho Vepsäläinen <bebraw@gmail.com>
2021-11-12 14:23:36 +0200

    chore: Expose dataSources

---
+++ dataSources/index.ts
@@ -0,0 +1,4 @@
+import file from "./file.ts";
+import indexMarkdown from "./indexMarkdown.ts";
+
+export { file, indexMarkdown };
--- mod.ts
+++ mod.ts
@@ -1,5 +1,6 @@
 import { build } from "./src/build.ts";
 import { serve } from "./src/serve.ts";
+import * as dataSources from "./dataSources/index.ts";
 import * as transforms from "./transforms/index.ts";
 
-export { build, serve, transforms };
+export { build, dataSources, serve, transforms };
