555f2ce14ef5d0235fe04399b791f1288dcd4e1e
Juho Vepsäläinen <bebraw@gmail.com>
2021-11-13 14:41:34 +0200

    chore: Add a test page for buttons

--- twindSetup.ts
+++ twindSetup.ts
@@ -3,5 +3,11 @@ import twindTypography from "https://cdn.skypack.dev/@twind/typography@0.0.2?min
 
 export default {
   theme: { extend: { colors: twindColors } },
-  plugins: twindTypography(),
+  plugins: {
+    btn: `font-bold py-2 px-4 rounded`,
+    "btn-blue": `bg-blue-500 text-white`,
+    "btn-blue:hover": `bg-blue-700`,
+    "btn-muted": `font-light text-gray-500`,
+    ...twindTypography(),
+  },
 };
