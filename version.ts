f7cee8e90aaa05560e07c5789f0f70a38ff336aa
Juho Vepsäläinen <bebraw@gmail.com>
2021-11-13 15:29:35 +0200

    Allow injecting only twind runtime

--- version.ts
+++ version.ts
@@ -1,6 +1,6 @@
 import { compileGustwindScripts } from "./compileGustwindScripts.ts";
 
-export const VERSION = "0.11.3";
+export const VERSION = "0.12.0";
 
 export async function prepublish(_version: string) {
   try {
