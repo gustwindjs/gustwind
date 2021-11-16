27d3f20f57f6c9dcce792748f805826dfaacee72
Juho Vepsäläinen <bebraw@gmail.com>
2021-11-16 17:57:15 +0200

    v0.13.4

--- version.ts
+++ version.ts
@@ -1,6 +1,6 @@
 import { compileGustwindScripts } from "./compileGustwindScripts.ts";
 
-export const VERSION = "0.13.3";
+export const VERSION = "0.13.4";
 
 export async function prepublish(_version: string) {
   try {
