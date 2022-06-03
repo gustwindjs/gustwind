2f8df23754995353b73e1065b2c73454006e1716
Juho Vepsäläinen <bebraw@gmail.com>
2022-06-03 13:31:32 +0300

    chore: Update dependencies

--- server-deps.ts
+++ server-deps.ts
@@ -1,10 +1,10 @@
-import * as async from "https://deno.land/std@0.113.0/async/mod.ts";
-import * as esbuild from "https://deno.land/x/esbuild@v0.13.12/mod.js";
-import * as fs from "https://deno.land/std@0.113.0/fs/mod.ts";
-import * as flags from "https://deno.land/std@0.113.0/flags/mod.ts";
+import * as async from "https://deno.land/std@0.142.0/async/mod.ts";
+import * as fs from "https://deno.land/std@0.142.0/fs/mod.ts";
+import * as flags from "https://deno.land/std@0.142.0/flags/mod.ts";
+import * as path from "https://deno.land/std@0.142.0/path/mod.ts";
+import * as yamlParse from "https://deno.land/std@0.142.0/encoding/yaml.ts";
 
-import * as path from "https://deno.land/std@0.113.0/path/mod.ts";
-import * as yaml from "https://esm.sh/yaml@1.10.2";
-import * as yamlParse from "https://deno.land/std@0.113.0/encoding/yaml.ts";
+import * as esbuild from "https://deno.land/x/esbuild@v0.14.42/mod.js";
+import * as yaml from "https://esm.sh/yaml@2.1.1";
 
 export { async, esbuild, flags, fs, path, yaml, yamlParse };
