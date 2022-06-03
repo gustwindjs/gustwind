d9e00f6e2b5547d8ffa9b76f8a017c8b59daa0af
Juho Vepsäläinen <bebraw@gmail.com>
2022-06-03 13:38:43 +0300

    chore: Move more dependencies to server deps

--- server-deps.ts
+++ server-deps.ts
@@ -1,11 +1,26 @@
 import * as async from "https://deno.land/std@0.142.0/async/mod.ts";
 import * as fs from "https://deno.land/std@0.142.0/fs/mod.ts";
 import * as flags from "https://deno.land/std@0.142.0/flags/mod.ts";
+import { lookup } from "https://deno.land/x/media_types@v3.0.3/mod.ts";
 import * as path from "https://deno.land/std@0.142.0/path/mod.ts";
 import * as yamlParse from "https://deno.land/std@0.142.0/encoding/yaml.ts";
+import { Server } from "https://deno.land/std@0.142.0/http/server.ts";
 
+import { cache } from "https://deno.land/x/cache@0.2.13/mod.ts";
 import * as esbuild from "https://deno.land/x/esbuild@v0.14.42/mod.js";
 import * as websockets from "https://deno.land/x/websocket@v0.1.4/mod.ts";
 import * as yaml from "https://esm.sh/yaml@2.1.1";
 
-export { async, esbuild, flags, fs, path, websockets, yaml, yamlParse };
+export {
+  async,
+  cache,
+  esbuild,
+  flags,
+  fs,
+  lookup,
+  path,
+  Server,
+  websockets,
+  yaml,
+  yamlParse,
+};
