1209e6b5e84f0e370528f035311903062e0f498b
Juho Vepsäläinen <bebraw@gmail.com>
2022-08-03 13:31:15 +0300

    fix: Fix export

--- mod.ts
+++ mod.ts
@@ -1,6 +1,6 @@
 import { build } from "./src/build.ts";
 import { serveGustwind } from "./src/serve.ts";
-import * as dataSources from "./site/dataSources/index.ts";
+import * as dataSources from "./site/dataSources.ts";
 import * as transforms from "./site/transforms/index.ts";
 
 export { build, dataSources, serveGustwind as serve, transforms };
