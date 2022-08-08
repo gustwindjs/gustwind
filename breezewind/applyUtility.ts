266bd5363c24d41d854cc95c0ef2805735e6c836
Juho Vepsäläinen <bebraw@gmail.com>
2022-08-08 10:20:28 +0300

    chore: Let utility parameters be async

--- breezewind/applyUtility.ts
+++ breezewind/applyUtility.ts
@@ -1,11 +1,11 @@
 import type { Context, Utilities, Utility } from "./types.ts";
 
-function applyUtility(
+async function applyUtility(
   value: Utility,
   utilities?: Utilities,
   context?: Context,
   // deno-lint-ignore no-explicit-any
-): any {
+): Promise<any> {
   if (!utilities) {
     throw new Error("applyUtility - No utilities were provided");
   }
@@ -17,21 +17,23 @@ function applyUtility(
     throw new Error("applyUtility - Matching utility was not found");
   }
 
+  const parameters = await Promise.all(
+    Array.isArray(value.parameters)
+      ? value.parameters.map((p) => {
+        if (typeof p === "string") {
+          // Nothing to do
+        } else if (p.utility && p.parameters) {
+          return applyUtility(p, utilities, context);
+        }
+
+        return p;
+      })
+      : [],
+  );
+
   return foundUtility.apply(
     null,
-    [context].concat(
-      Array.isArray(value.parameters)
-        ? value.parameters.map((p) => {
-          if (typeof p === "string") {
-            // Nothing to do
-          } else if (p.utility && p.parameters) {
-            return applyUtility(p, utilities, context);
-          }
-
-          return p;
-        })
-        : [],
-    ),
+    [context].concat(parameters),
   );
 }
 
