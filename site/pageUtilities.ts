fbc0b2b57029f07d7a54e0c3ec3d37a94d0aabb9
Juho Vepsäläinen <bebraw@gmail.com>
2022-08-12 09:51:27 +0300

    chore: Use the correct type

--- site/pageUtilities.ts
+++ site/pageUtilities.ts
@@ -1,19 +1,20 @@
 import md from "./transforms/markdown.ts";
 import { tw as twind } from "../client-deps.ts";
+import type { Context } from "../breezewind/types.ts";
 
-function dateToISO(_: unknown, date: string) {
+function dateToISO(_: Context, date: string) {
   return (new Date(date)).toISOString();
 }
 
-function markdown(_: unknown, input: string) {
+function markdown(_: Context, input: string) {
   return md(input).content;
 }
 
-function testUtility(_: unknown, input: string) {
+function testUtility(_: Context, input: string) {
   return input;
 }
 
-function tw(_: unknown, input: string) {
+function tw(_: Context, input: string) {
   return twind(input);
 }
 
