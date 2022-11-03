9c4d647046960a74fca859c98f56b2b07e39e7fc
Juho Vepsäläinen <bebraw@gmail.com>
2022-11-03 09:40:46 +0200

    chore: Update dependencies

--- server-deps.ts
+++ server-deps.ts
@@ -1,16 +1,15 @@
-import * as async from "https://deno.land/std@0.151.0/async/mod.ts";
-import * as fs from "https://deno.land/std@0.151.0/fs/mod.ts";
-import * as flags from "https://deno.land/std@0.151.0/flags/mod.ts";
+import * as async from "https://deno.land/std@0.161.0/async/mod.ts";
+import * as fs from "https://deno.land/std@0.161.0/fs/mod.ts";
+import * as flags from "https://deno.land/std@0.161.0/flags/mod.ts";
 import { lookup } from "https://deno.land/x/media_types@v3.0.3/mod.ts";
-import * as path from "https://deno.land/std@0.151.0/path/mod.ts";
-import * as yamlParse from "https://deno.land/std@0.151.0/encoding/yaml.ts";
-import { Server } from "https://deno.land/std@0.151.0/http/server.ts";
-
+import * as path from "https://deno.land/std@0.161.0/path/mod.ts";
+import * as yamlParse from "https://deno.land/std@0.161.0/encoding/yaml.ts";
+import { Server } from "https://deno.land/std@0.161.0/http/server.ts";
 import { cache } from "https://deno.land/x/cache@0.2.13/mod.ts";
-import * as esbuild from "https://deno.land/x/esbuild@v0.14.54/mod.js";
+import * as esbuild from "https://deno.land/x/esbuild@v0.15.13/mod.js";
 import { nanoid } from "https://cdn.skypack.dev/nanoid@4.0.0?min";
 import * as websockets from "https://deno.land/x/websocket@v0.1.4/mod.ts";
-import * as yaml from "https://esm.sh/yaml@2.1.1";
+import * as yaml from "https://esm.sh/yaml@2.1.3";
 
 export {
   async,
