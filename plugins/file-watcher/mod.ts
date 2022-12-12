6d1ebee99aec222ac8b7acc12688acb5c0471ce8
Juho Vepsäläinen <bebraw@gmail.com>
2022-12-12 13:27:31 +0200

    chore: Set up todos

--- plugins/file-watcher/mod.ts
+++ plugins/file-watcher/mod.ts
@@ -1,5 +1,16 @@
-// TODO: Set up a file watcher here
-// It should be able to aggregate files to watch automatically
-// and trigger a message when there's a change.
-// Then another plugin (for example web socket plugin), can
-// react to that and update the clients.
+import type { Plugin, PluginMeta } from "../../types.ts";
+
+const meta: PluginMeta = {
+  name: "gustwind-script-plugin",
+};
+
+function fileWatcherPlugin(): Plugin {
+  // TODO: Set up a file watcher here
+  // It should be able to aggregate files to watch automatically
+  // and trigger a message when there's a change.
+  // Then another plugin (for example web socket plugin), can
+  // react to that and update the clients.
+  return {};
+}
+
+export { fileWatcherPlugin as plugin, meta };
