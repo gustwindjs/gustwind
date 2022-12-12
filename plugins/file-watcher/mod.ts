582f94866900673a874e68789868e665d90b9e1b
Juho Vepsäläinen <bebraw@gmail.com>
2022-12-12 13:22:53 +0200

    chore: Add a todo

---
+++ plugins/file-watcher/mod.ts
@@ -0,0 +1,5 @@
+// TODO: Set up a file watcher here
+// It should be able to aggregate files to watch automatically
+// and trigger a message when there's a change.
+// Then another plugin (for example web socket plugin), can
+// react to that and update the clients.
