23c3f9ef848b646ce1fddf8a88bbfbeb8c2cabb0
Juho Vepsäläinen <bebraw@gmail.com>
2022-12-20 13:59:22 +0200

    chore: Drop redundant export

--- client-deps.ts
+++ client-deps.ts
@@ -1,4 +1,3 @@
-import { setup as setupTwind } from "https://cdn.skypack.dev/twind@0.16.16?min";
 import { tw } from "https://cdn.skypack.dev/twind@0.16.16?min";
 
-export { setupTwind, tw };
+export { tw };
