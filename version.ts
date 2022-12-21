31ba7bc61d06204675dd0dfe8770574e4d05fdff
Juho Vepsäläinen <bebraw@gmail.com>
2022-12-21 17:16:08 +0200

    v0.32.2-alpha.9

--- version.ts
+++ version.ts
@@ -1,6 +1,6 @@
 import { compilePlugins } from "./compilePluginScripts.ts";
 
-export const VERSION = "0.32.2-alpha.8";
+export const VERSION = "0.32.2-alpha.9";
 
 export async function prepublish(_version: string) {
   // TODO: Run breezewind tests here
