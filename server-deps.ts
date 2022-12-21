c010ebe4e0d5fe9848c2d93d2e600e88c54d692b
Juho Vepsäläinen <bebraw@gmail.com>
2022-12-21 16:11:50 +0200

    chore: Update esbuild

--- server-deps.ts
+++ server-deps.ts
@@ -5,7 +5,7 @@ import { lookup } from "https://deno.land/x/media_types@v3.0.3/mod.ts";
 import * as path from "https://deno.land/std@0.161.0/path/mod.ts";
 import * as yamlParse from "https://deno.land/std@0.161.0/encoding/yaml.ts";
 import { Server } from "https://deno.land/std@0.161.0/http/server.ts";
-import * as esbuild from "https://deno.land/x/esbuild@v0.15.13/mod.js";
+import * as esbuild from "https://deno.land/x/esbuild@v0.16.10/mod.js";
 import { nanoid } from "https://cdn.skypack.dev/nanoid@4.0.0?min";
 import * as websockets from "https://deno.land/x/websocket@v0.1.4/mod.ts";
 
