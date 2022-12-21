b73b06e0595167d807ce4e5648c11488417ade17
Juho Vepsäläinen <bebraw@gmail.com>
2022-12-21 17:00:14 +0200

    chore: Drop cwd test

--- gustwind-cli/mod.ts
+++ gustwind-cli/mod.ts
@@ -15,8 +15,6 @@ import { getWebsocketServer } from "../utilities/getWebSocketServer.ts";
 import { plugin as fileWatcherPlugin } from "../plugins/file-watcher/mod.ts";
 import { plugin as webSocketPlugin } from "../plugins/websocket/mod.ts";
 
-const DEBUG = Deno.env.get("DEBUG") === "1";
-
 function usage() {
   console.log(`
 Usage: gustwind [-b|-d] [-D] [-p <port>] [-t <number|"cpuMax"|"cpuHalf">] [-o <directory>]
@@ -89,8 +87,6 @@ export async function main(cliArgs: string[]): Promise<number | undefined> {
   }
 
   const cwd = Deno.cwd();
-
-  DEBUG && console.log(`cwd is ${cwd}`);
 
   // TODO: Allow passing this as a parameter
   const pluginsPath = path.join(cwd, "plugins.json");
