701eebc9a80acd6b70e49a47687742a015c1bdfc
Juho Vepsäläinen <bebraw@gmail.com>
2023-03-09 16:57:59 +0200

    v0.39.9

--- version.ts
+++ version.ts
@@ -1,6 +1,6 @@
 import { compilePlugins } from "./compilePluginScripts.ts";
 
-export const VERSION = "0.39.8";
+export const VERSION = "0.39.9";
 
 export async function prepublish(_version: string) {
   // TODO: Run breezewind tests here
