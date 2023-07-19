48fd53545ef60f7223366ae8ce37530cda2353d1
Juho Vepsäläinen <bebraw@gmail.com>
2023-07-19 12:27:42 +0300

    chore: Sketch out component handling

--- html-to-breezewind/tests/component_test.ts
+++ html-to-breezewind/tests/component_test.ts
@@ -1,2 +1,19 @@
+import { assertEquals } from "https://deno.land/std@0.142.0/testing/asserts.ts";
+import htmlToBreezewind from "../index.ts";
+
+Deno.test("basic component", () => {
+  assertEquals(
+    // TODO: the problem is that HTML tagName loses original casing
+    htmlToBreezewind(`<Button>foo</Button>`),
+    {
+      type: "Button",
+      children: "foo",
+      attributes: {},
+    },
+  );
+});
+
 // TODO: Test a component
+// TODO: Test prop handling
+// TODO: Test prop children
 // TODO: Test Noop
