31f4c66c15cceafea4788e14daf3714543fcdf8a
Juho Vepsäläinen <bebraw@gmail.com>
2023-11-02 10:28:56 +0200

    refactor: Extract Performance component

---
+++ site/components/Performance.ts
@@ -0,0 +1,26 @@
+import type { Context } from "../../breezewind/types.ts";
+
+function init() {
+  let renderStart: number;
+
+  function _onRenderStart() {
+    // This is triggered when page rendering begins.
+    // It's a good spot for clearing ids caches (think anchoring)
+    // or benchmarking.
+    renderStart = performance.now();
+  }
+
+  function _onRenderEnd(context: Context) {
+    if (context.pagePath) {
+      const renderEnd = performance.now();
+
+      console.log(
+        `Rendered ${context.pagePath} in ${renderEnd - renderStart} ms.`,
+      );
+    }
+  }
+
+  return { _onRenderEnd, _onRenderStart };
+}
+
+export { init };
--- site/pageUtilities.ts
+++ site/pageUtilities.ts
@@ -1,28 +1,7 @@
-import type { Context } from "../breezewind/types.ts";
-
 // init({ routes }: { routes: Routes })
 function init() {
-  // TODO: Move this logic to <Performance />
-  let renderStart: number;
-
-  function _onRenderStart() {
-    // This is triggered when page rendering begins.
-    // It's a good spot for clearing ids caches (think anchoring)
-    // or benchmarking.
-    renderStart = performance.now();
-  }
-
-  function _onRenderEnd(context: Context) {
-    if (context.pagePath) {
-      const renderEnd = performance.now();
-
-      console.log(
-        `Rendered ${context.pagePath} in ${renderEnd - renderStart} ms.`,
-      );
-    }
-  }
-
-  return { _onRenderEnd, _onRenderStart };
+  // Globally available utilities should be exposed here
+  return {};
 }
 
 export { init };
