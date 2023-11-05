b89a307b180cfd3f572db902eec255869f8d5b45
Juho Vepsäläinen <bebraw@gmail.com>
2023-11-05 13:20:37 +0200

    chore: Update dependencies

--- server-deps.ts
+++ server-deps.ts
@@ -1,7 +1,7 @@
 // Only shared dependencies should go here as otherwise CI
 // will install too many for a simple build.
-import * as fs from "https://deno.land/std@0.161.0/fs/mod.ts";
-import * as path from "https://deno.land/std@0.161.0/path/mod.ts";
-import { nanoid } from "https://cdn.skypack.dev/nanoid@4.0.0?min";
+import * as fs from "https://deno.land/std@0.205.0/fs/mod.ts";
+import * as path from "https://deno.land/std@0.205.0/path/mod.ts";
+import { nanoid } from "https://cdn.skypack.dev/nanoid@5.0.2?min";
 
 export { fs, nanoid, path };
