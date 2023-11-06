83354e862e6904d26d124451bee6e88404182bb9
Juho Vepsäläinen <bebraw@gmail.com>
2023-11-06 17:27:38 +0200

    chore: Drop data sources from the public interface

--- mod.ts
+++ mod.ts
@@ -1,6 +1,5 @@
 import { build } from "./gustwind-builder/mod.ts";
 import { gustwindDevServer } from "./gustwind-dev-server/mod.ts";
 import { gustwindServe } from "./gustwind-serve/mod.ts";
-import * as dataSources from "./site/dataSources.ts";
 
-export { build, dataSources, gustwindDevServer, gustwindServe };
+export { build, gustwindDevServer, gustwindServe };
