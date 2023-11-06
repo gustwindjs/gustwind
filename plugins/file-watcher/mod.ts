b0aa57596f83d69cdec9616ca7ac42d178604c55
Juho Vepsäläinen <bebraw@gmail.com>
2023-11-06 18:10:20 +0200

    chore: Extract watch utility

--- plugins/file-watcher/mod.ts
+++ plugins/file-watcher/mod.ts
@@ -1,5 +1,5 @@
 import { path as _path } from "../../server-deps.ts";
-import * as async from "https://deno.land/std@0.161.0/async/mod.ts";
+import { watch } from "../../utilities/watch.ts";
 import type { Plugin } from "../../types.ts";
 
 const DEBUG = Deno.env.get("DEBUG") === "1";
@@ -74,40 +74,5 @@ const plugin: Plugin<{ pluginsPath: string }> = {
     };
   },
 };
-
-async function watch({
-  paths,
-  onChange,
-  debounceDelay,
-}: {
-  paths: string[];
-  onChange: (path: string, event: Deno.FsEvent) => void;
-  debounceDelay?: number;
-}) {
-  // The watcher will crash hard if there's even a single invalid path
-  const pathsToWatch = (await Promise.all(
-    paths.filter(Boolean).filter((p) => !p.startsWith("http")).map(
-      async (p) => {
-        try {
-          await Deno.lstat(p);
-
-          return p;
-        } catch (_) {
-          return;
-        }
-      },
-    ),
-  )).filter(Boolean) as string[];
-
-  const watcher = Deno.watchFs(pathsToWatch);
-  const trigger = async.debounce(
-    (event: Deno.FsEvent) => event.paths.forEach((p) => onChange(p, event)),
-    debounceDelay || 200,
-  );
-
-  for await (const event of watcher) {
-    trigger(event);
-  }
-}
 
 export { plugin };
