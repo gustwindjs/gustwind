3d342bf7f41d8bc1ddf2a7cf9859f4adc7270236
Juho Vepsäläinen <bebraw@gmail.com>
2023-11-09 10:54:59 +0200

    chore: Add a complex noop test

--- html-to-breezewind/tests/noop_test.ts
+++ html-to-breezewind/tests/noop_test.ts
@@ -96,3 +96,58 @@ Deno.test("noop with children", () => {
     },
   );
 });
+
+Deno.test("nested noops", () => {
+  assertEquals(
+    htmlToBreezewind(`<noop
+      &visibleIf="(get props children)"
+      &type="(concat h (get props level))"
+      &id="(getUniqueAnchorId (get props children))"
+      &class="(get props class)"
+    >
+      <noop &children="(render (get props children))" />
+      <a
+        &visibleIf="(invert (get props hideAnchor))"
+        class="ml-2 no-underline text-sm align-middle mask-text-gray hover:mask-text-black"
+        &href="(concat # (get props id))"
+        >🔗</a
+      >
+    </noop>`),
+    {
+      visibleIf: { utility: "get", parameters: ["props", "children"] },
+      type: {
+        utility: "concat",
+        parameters: ["h", { utility: "get", parameters: ["props", "level"] }],
+      },
+      attributes: {
+        id: {
+          utility: "getUniqueAnchorId",
+          parameters: [{ utility: "get", parameters: ["props", "children"] }],
+        },
+        class: { utility: "get", parameters: ["props", "class"] },
+      },
+      children: [{
+        attributes: {},
+        children: {
+          utility: "render",
+          parameters: [{ utility: "get", parameters: ["props", "children"] }],
+        },
+      }, {
+        type: "a",
+        visibleIf: {
+          utility: "invert",
+          parameters: [{ utility: "get", parameters: ["props", "hideAnchor"] }],
+        },
+        attributes: {
+          class:
+            "ml-2 no-underline text-sm align-middle mask-text-gray hover:mask-text-black",
+          href: {
+            utility: "concat",
+            parameters: ["#", { utility: "get", parameters: ["props", "id"] }],
+          },
+        },
+        children: "🔗",
+      }],
+    },
+  );
+});
