4c53126bc323d10df26c127a8c305c9807a4d8e9
Juho Vepsäläinen <bebraw@gmail.com>
2023-11-27 13:39:17 +0200

    chore: Drop redundant info

--- site/components/Markdown.server.ts
+++ site/components/Markdown.server.ts
@@ -1,10 +1,6 @@
 import getMarkdown from "../transforms/markdown.ts";
 import type { LoadApi } from "../../types.ts";
 
-// The component/browser version should not use the regular transform
-// as that relies on filesystem related functionality which cannot
-// work in the browser. In other words, the component version is
-// more limited than the transform.
 function init({ load }: { load: LoadApi }) {
   const markdown = getMarkdown(load);
 
