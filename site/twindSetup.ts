818edb1691677a06a19bd0a8cdfc41ba09f2b094
Juho Vepsäläinen <bebraw@gmail.com>
2024-01-23 16:26:50 +0200

    fix: Fix button styles

--- site/twindSetup.ts
+++ site/twindSetup.ts
@@ -5,12 +5,10 @@ import meta from "./meta.json" assert { type: "json" };
 
 export default {
   presets: [presetAutoprefix(), presetTailwind(), presetTypography()],
-  plugins: {
-    btn: "font-bold py-2 px-4 rounded",
-    "btn-blue": "bg-blue-500 hover:bg-blue-700 text-white",
-    "btn-muted": "font-light text-gray-500",
-  },
   rules: [
+    ["btn", "font-bold py-2 px-4 rounded"],
+    ["btn-blue", "bg-blue-500 hover:bg-blue-700 text-white"],
+    ["btn-muted", "font-light text-gray-500"],
     // https://twind.style/rules#static-rules
     ["mask-text", { color: "transparent", textShadow: "0 0 black" }],
     // For navigation
