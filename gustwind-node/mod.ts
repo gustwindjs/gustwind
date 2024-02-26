ce9fb0ee84be22b116b7466d494c59b95fec41f2
Juho Vepsäläinen <bebraw@gmail.com>
2024-02-26 11:47:38 +0200

    chore: Sketch out build

--- gustwind-node/mod.ts
+++ gustwind-node/mod.ts
@@ -1,13 +1,32 @@
 import { path } from "../server-deps.ts";
+import { applyPlugins, importPlugins } from "../gustwind-utilities/plugins.ts";
 import { evaluatePluginsDefinition } from "../utilities/evaluatePluginsDefinition.ts";
 
 async function build(pluginsLookupPath: string) {
   const cwd = Deno.cwd();
   const pluginsPath = path.join(cwd, pluginsLookupPath || "plugins.json");
   const pluginDefinitions = await evaluatePluginsDefinition(pluginsPath);
+  const { plugins } = await importPlugins({
+    cwd,
+    pluginDefinitions,
+    outputDirectory: "",
+    mode: "production",
+  });
 
-  // TODO: Redefine this, maybe some part should be extracted from the build worker to share
-  //await buildProject({ cwd, outputDirectory, pluginDefinitions, threads });
+  // TODO: Is route the correct abstraction or should there be a way to go through
+  // templating logic directly? -> another way to apply plugins?
+  // TODO: What to do with possibly remaining tasks
+  const { markup, tasks } = await applyPlugins({
+    plugins,
+    // TODO: Pass url as a parameter?
+    url: "",
+    // TODO: Get routes definition
+    routes: {},
+    // TODO: Pass route as a parameter?
+    route: { context: {}, url: "", layout: "", meta: {} },
+  });
+
+  return markup;
 }
 
 export { build };
