02861deeaba6d37eeccdee0c509222cc81ecd03f
Juho Vepsäläinen <bebraw@gmail.com>
2024-02-26 13:36:21 +0200

    chore: Add missing bits to build

--- gustwind-node/mod.ts
+++ gustwind-node/mod.ts
@@ -2,31 +2,35 @@ import { path } from "../server-deps.ts";
 import { applyPlugins, importPlugins } from "../gustwind-utilities/plugins.ts";
 import { evaluatePluginsDefinition } from "../utilities/evaluatePluginsDefinition.ts";
 
-async function build(pluginsLookupPath: string) {
+async function build(pluginsLookupPath: string, url: string) {
   const cwd = Deno.cwd();
   const pluginsPath = path.join(cwd, pluginsLookupPath || "plugins.json");
   const pluginDefinitions = await evaluatePluginsDefinition(pluginsPath);
-  const { plugins } = await importPlugins({
+  const { plugins, router } = await importPlugins({
     cwd,
     pluginDefinitions,
     outputDirectory: "",
     mode: "production",
   });
+  const { routes, tasks: initialTasks } = await router.getAllRoutes();
+  const route = routes[url];
 
-  // TODO: Is route the correct abstraction or should there be a way to go through
-  // templating logic directly? -> another way to apply plugins?
-  // TODO: What to do with possibly remaining tasks
-  const { markup, tasks } = await applyPlugins({
+  if (!route) {
+    throw new Error(`Matching route was not found for ${url}!`);
+  }
+
+  const { markup, tasks: routeTasks } = await applyPlugins({
     plugins,
-    // TODO: Pass url as a parameter?
-    url: "",
-    // TODO: Get routes definition
-    routes: {},
-    // TODO: Pass route as a parameter?
-    route: { context: {}, url: "", layout: "", meta: {} },
+    url,
+    routes,
+    route,
   });
 
-  return markup;
+  // Both markup and tasks are returned. The idea is that the consumer can
+  // decide what to do with either. For example on edge you might want to
+  // handle the tasks dynamically by writing task results to KV and then
+  // map to the database through a router on demand.
+  return { markup, tasks: initialTasks.concat(routeTasks) };
 }
 
 export { build };
