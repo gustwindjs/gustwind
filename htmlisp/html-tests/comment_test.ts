6f986048f4395201219d00e14060f4a3a9434639
Juho Vepsäläinen <bebraw@gmail.com>
2024-03-09 10:24:38 +0200

    feat: Support comment syntax

--- htmlisp/html-tests/comment_test.ts
+++ htmlisp/html-tests/comment_test.ts
@@ -1,15 +1,11 @@
 import { assertEquals } from "https://deno.land/std@0.142.0/testing/asserts.ts";
-import { htmlispToBreezewind } from "../mod.ts";
+import { htmlispToHTML } from "../mod.ts";
 
 Deno.test("element with a comment", () => {
   assertEquals(
-    htmlispToBreezewind(
+    htmlispToHTML(
       `<div __reference="https://kevincox.ca/2022/05/06/rss-feed-best-practices/">foo</div>`,
     ),
-    {
-      type: "div",
-      children: "foo",
-      attributes: {},
-    },
+    "<div>foo</div>",
   );
 });
