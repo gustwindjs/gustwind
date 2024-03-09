588ba8cc0d6d2024177a0ac75d2b53dd0c88db4a
Juho Vepsäläinen <bebraw@gmail.com>
2024-03-09 10:07:40 +0200

    chore: Rename converter

--- htmlisp/mod.ts
+++ htmlisp/mod.ts
@@ -1,5 +1,5 @@
 import htm from "https://esm.sh/htm@3.1.1";
-import { getConverter } from "./convert.ts";
+import { getConverter } from "./convert-to-gustwind.ts";
 
 // @ts-expect-error I'm not sure how to type this but it doesn't matter
 const htmlToBreezewind = getConverter(htm);
