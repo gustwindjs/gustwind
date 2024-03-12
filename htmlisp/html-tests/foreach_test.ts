0213f27a1b61715e4a3c567b413d2d1271121ce7
Juho Vepsäläinen <bebraw@gmail.com>
2024-03-12 15:13:05 +0200

    chore: Sketch out an alternative

--- htmlisp/html-tests/foreach_test.ts
+++ htmlisp/html-tests/foreach_test.ts
@@ -1,6 +1,31 @@
-import { urlJoin } from "https://deno.land/x/url_join@1.0.0/mod.ts";
 import { assertEquals } from "https://deno.land/std@0.142.0/testing/asserts.ts";
 import { htmlispToHTML } from "../mod.ts";
+
+// TODO: Try this option
+/*
+Deno.test("Foreach with an array of objects", async () => {
+  assertEquals(
+    await htmlispToHTML({
+      htmlInput: `<foreach type="ul" values="(get context blogPosts)">
+        <li class="inline" &title="(get props data.title)" &children="(get props data.content)">
+        </li>
+      </foreach>
+    `,
+      context: {
+        blogPosts: [{ title: "foo", content: "bar" }],
+      },
+      components: {
+        Foreach:
+          `<noop &type="(get props type)" &children="(join (get props values) ' ')"></noop>`,
+      },
+      utilities: {
+        join: (values: unknown[], separator: string) => values.join(separator),
+      },
+    }),
+    `<ul><li class="inline" title="foo">bar</li></ul>`,
+  );
+});
+*/
 
 // TODO: Restore
 Deno.test("&foreach with an array of objects", async () => {
@@ -14,7 +39,6 @@ Deno.test("&foreach with an array of objects", async () => {
       context: {
         blogPosts: [{ title: "foo", content: "bar" }],
       },
-      utilities: { urlJoin },
     }),
     `<ul><li class="inline" title="foo">bar</li></ul>`,
   );
@@ -32,7 +56,6 @@ Deno.test("&foreach with an array of objects and an attribute", async () => {
       context: {
         blogPosts: [{ title: "foo", content: "bar" }],
       },
-      utilities: { urlJoin },
     }),
     `<ul title="demo"><li class="inline" title="foo">bar</li></ul>`,
   );
@@ -52,7 +75,6 @@ Deno.test("&foreach with an array of objects and an evaluated attribute", async 
         demo: "demo",
         blogPosts: [{ title: "foo", content: "bar" }],
       },
-      utilities: { urlJoin },
     }),
     `<ul title="demo"><li class="inline" title="foo">bar</li></ul>`,
   );
@@ -70,7 +92,6 @@ Deno.test("&foreach with an array of values", async () => {
       context: {
         blogPosts: ["foo"],
       },
-      utilities: { urlJoin },
     }),
     `<ul><li class="inline">foo</li></ul>`,
   );
