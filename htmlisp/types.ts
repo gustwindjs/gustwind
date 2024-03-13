ad7880f198f238828f455ee08ac945e79e00f41d
Juho Vepsäläinen <bebraw@gmail.com>
2024-03-13 15:45:24 +0200

    chore: Sketch out Foreach

---
+++ htmlisp/types.ts
@@ -0,0 +1,6 @@
+type Attributes = Record<string, string> | null;
+type Components = Record<string, string>;
+type Context = Record<string, unknown>;
+// type Utilities = Record<string, (...args: any) => unknown>;
+
+export type { Attributes, Components, Context };
