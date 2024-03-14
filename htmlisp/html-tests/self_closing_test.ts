dcf7cb0b63a4cb0add8cf75bdc57eb156c29ba5c
Juho Vepsäläinen <bebraw@gmail.com>
2024-03-14 18:56:13 +0200

    refactor: Extract self-closing tests

---
+++ htmlisp/html-tests/self_closing_test.ts
@@ -0,0 +1,30 @@
+import { assertEquals } from "https://deno.land/std@0.142.0/testing/asserts.ts";
+import { htmlispToHTML } from "../mod.ts";
+
+Deno.test("self-closing element without whitespace", async () => {
+  assertEquals(
+    await htmlispToHTML({ htmlInput: `<hr/>` }),
+    `<hr/>`,
+  );
+});
+
+Deno.test("self-closing element with whitespace", async () => {
+  assertEquals(
+    await htmlispToHTML({ htmlInput: `<hr />` }),
+    `<hr/>`,
+  );
+});
+
+Deno.test("self-closing element with an attribute", async () => {
+  assertEquals(
+    await htmlispToHTML({ htmlInput: `<div title="bar"/>` }),
+    `<div title="bar"/>`,
+  );
+});
+
+Deno.test("self-closing element with an attribute and whitespace", async () => {
+  assertEquals(
+    await htmlispToHTML({ htmlInput: `<div title="bar" />` }),
+    `<div title="bar"/>`,
+  );
+});
