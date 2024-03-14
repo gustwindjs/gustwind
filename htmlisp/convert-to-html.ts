5612f7beb30c9a8d6ab042f595f9f61a490f087d
Juho Vepsäläinen <bebraw@gmail.com>
2024-03-14 10:20:46 +0200

    refactor: Extract parser

--- htmlisp/convert-to-html.ts
+++ htmlisp/convert-to-html.ts
@@ -3,6 +3,7 @@ import { parseExpressions } from "./utilities/parseExpressions.ts";
 import { parseExpression } from "./utilities/parseExpression.ts";
 import { defaultUtilities } from "../breezewind/defaultUtilities.ts";
 import { applyUtility } from "../breezewind/applyUtility.ts";
+import { parseHtmlisp } from "./utilities/parseHtmlisp.ts";
 import type { Utilities, Utility } from "../breezewind/types.ts";
 import type { Attributes, Components, Context } from "./types.ts";
 
@@ -83,24 +84,6 @@ function getConverter(
     */
   };
 }
-/*
-const NOT_PARSING = 0;
-const PARSE_TAG_START = 1;
-const PARSE_BODY = 2;
-const PARSE_ATTRIBUTE_NAME = 3;
-const PARSE_ATTRIBUTE_VALUE = 4;
-const PARSE_TAG_END = 5;
-*/
-
-// Debug helpers
-const NOT_PARSING = "not parsing";
-const PARSE_TAG_START = "parse tag start";
-const PARSE_BODY = "parse body";
-const PARSE_ATTRIBUTE_NAME = "parse attribute name";
-const PARSE_ATTRIBUTE_VALUE = "parse attribute value";
-const PARSE_TAG_END = "parse tag end";
-
-type Attribute = { name: string; value: string };
 
 function getConvertToHTML(
   components: Components,
@@ -112,81 +95,11 @@ function getConvertToHTML(
 ) {
   return function convert(input: string) {
     const ret: string[] = [];
-    let parsingState = NOT_PARSING;
-    let quotesFound = 0;
-    let capturedTag: {
-      name: string;
-      attributes: Attribute[];
-      // TODO: Potentially add children here
-      children: string;
-    } = { name: "", attributes: [], children: "" };
-    let capturedAttribute: Attribute = { name: "", value: "" };
-    const capturedTags = [];
+    const parsedHtmlisp = parseHtmlisp(input);
 
-    for (let i = 0; i < input.length; i++) {
-      const c = input[i];
+    console.log(parsedHtmlisp);
 
-      // Debug helper
-      // console.log(parsingState, c);
-
-      if (parsingState === NOT_PARSING) {
-        if (c === "<") {
-          parsingState = PARSE_TAG_START;
-        } else if (c === ">") {
-          parsingState = PARSE_BODY;
-        }
-      } else if (parsingState === PARSE_TAG_START) {
-        if (c === ">") {
-          parsingState = PARSE_BODY;
-        } else if (c === " ") {
-          parsingState = PARSE_ATTRIBUTE_NAME;
-          capturedAttribute = { name: "", value: "" };
-        } else {
-          capturedTag.name += c;
-        }
-      } else if (parsingState === PARSE_ATTRIBUTE_NAME) {
-        if (c === "=") {
-          parsingState = PARSE_ATTRIBUTE_VALUE;
-        } else {
-          capturedAttribute.name += c;
-        }
-      } else if (parsingState === PARSE_ATTRIBUTE_VALUE) {
-        if (c === '"') {
-          quotesFound++;
-
-          if (quotesFound === 2) {
-            capturedTag.attributes.push(capturedAttribute);
-            parsingState = NOT_PARSING;
-            quotesFound = 0;
-          }
-        } else {
-          capturedAttribute.value += c;
-        }
-      } else if (parsingState === PARSE_BODY) {
-        if (c === "<") {
-          parsingState = PARSE_TAG_END;
-        } else {
-          capturedTag.children += c;
-        }
-      } else if (parsingState === PARSE_TAG_END) {
-        if (c === ">") {
-          parsingState = NOT_PARSING;
-          capturedTags.push(structuredClone(capturedTag));
-          capturedTag = {
-            name: "",
-            attributes: [],
-            children: "",
-          };
-          quotesFound = 0;
-
-          // TODO: This would be the spot to convert already most likely
-        }
-      }
-    }
-
-    // TODO: Comment - __ - potentially this could be done when committing
-    console.log(capturedTags);
-
+    // TODO: Stringify parsed structure
     return ret.join("");
   };
 }
