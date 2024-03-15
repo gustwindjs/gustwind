98e1c1550d1f96e877030413d71e3b54985b1204
Juho Vepsäläinen <bebraw@gmail.com>
2024-03-15 08:23:38 +0200

    chore: Drop redundant code

--- htmlisp/mod.ts
+++ htmlisp/mod.ts
@@ -1,8 +1,5 @@
-import { htm } from "./utilities/htm.js";
-import { getConverter as getBreezewindConverter } from "./convert-to-gustwind.ts";
 import { getConverter as getHTMLConverter } from "./convert-to-html.ts";
 
-const htmlispToBreezewind = getBreezewindConverter(htm);
 const htmlispToHTML = getHTMLConverter();
 
-export { htmlispToBreezewind, htmlispToHTML };
+export { htmlispToHTML };
