fc94d370965bda6fb71bb32acc47fc5457c71fe5
Juho Vepsäläinen <bebraw@gmail.com>
2024-03-16 12:26:11 +0200

    chore: Add self-closing tests

--- htmlisp/html-tests/element_test.ts
+++ htmlisp/html-tests/element_test.ts
@@ -126,6 +126,41 @@ Deno.test("element with an attribute", async () => {
   );
 });
 
+Deno.test("element with a self-closing attribute", async () => {
+  assertEquals(
+    await htmlispToHTML({ htmlInput: `<div demo title="bar">foo</div>` }),
+    `<div demo title="bar">foo</div>`,
+  );
+});
+
+Deno.test("element with a self-closing attribute at the end", async () => {
+  assertEquals(
+    await htmlispToHTML({ htmlInput: `<div title="bar" demo>foo</div>` }),
+    `<div title="bar" demo>foo</div>`,
+  );
+});
+
+Deno.test("self-closing element with an attribute", async () => {
+  assertEquals(
+    await htmlispToHTML({ htmlInput: `<div title="bar" />` }),
+    `<div title="bar"/>`,
+  );
+});
+
+Deno.test("self-closing element with a self-closing attribute", async () => {
+  assertEquals(
+    await htmlispToHTML({ htmlInput: `<div demo title="bar" />` }),
+    `<div demo title="bar" />`,
+  );
+});
+
+Deno.test("self-closing element with a self-closing attribute at the end", async () => {
+  assertEquals(
+    await htmlispToHTML({ htmlInput: `<div title="bar" demo />` }),
+    `<div title="bar" demo/>`,
+  );
+});
+
 Deno.test("element with multiple attributes", async () => {
   assertEquals(
     await htmlispToHTML({ htmlInput: `<div title="bar" alt="foo">foo</div>` }),
