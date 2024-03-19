c5cf4065fe9c656da088d15cfb6fbc7c12fd21d0
Juho Vepsäläinen <bebraw@gmail.com>
2024-03-19 09:23:01 +0200

    chore: Add render specification

---
+++ htmlisp/html-tests/render_test.ts
@@ -0,0 +1,17 @@
+import { assertEquals } from "https://deno.land/std@0.142.0/testing/asserts.ts";
+import { htmlispToHTML } from "../mod.ts";
+
+Deno.test("component with render", async () => {
+  assertEquals(
+    await htmlispToHTML({
+      htmlInput: `<div &children="(render (get context button))"></div>`,
+      components: {
+        Button: '<button &children="(get props children)"></button>',
+      },
+      context: {
+        button: '<Button &children="demo" />',
+      },
+    }),
+    `<div><button>demo</button></div>`,
+  );
+});
