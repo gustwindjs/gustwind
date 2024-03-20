05fb99c0736cf9ae6c86951854c1e560e53e6330
Juho Vepsäläinen <bebraw@gmail.com>
2024-03-20 14:50:27 +0200

    chore: Extract tests

--- htmlisp/html-tests/element_test.ts
+++ htmlisp/html-tests/element_test.ts
@@ -38,44 +38,6 @@ Deno.test("basic element with nested children", async () => {
   );
 });
 
-Deno.test("basic element with siblings as children", async () => {
-  assertEquals(
-    await htmlispToHTML({
-      htmlInput:
-        `<div title="demo"><span>foobar</span><span>barfoo</span></div>`,
-    }),
-    `<div title="demo"><span>foobar</span><span>barfoo</span></div>`,
-  );
-});
-
-Deno.test("basic element with siblings as children 2", async () => {
-  assertEquals(
-    await htmlispToHTML({
-      htmlInput: `<div
-      title="demo"
-    >
-      <span>foobar</span>
-      <span>barfoo</span>
-</div>`,
-    }),
-    `<div title="demo"><span>foobar</span><span>barfoo</span></div>`,
-  );
-});
-
-Deno.test("basic element with siblings as children 3", async () => {
-  assertEquals(
-    await htmlispToHTML({
-      htmlInput: `<div
-      title="demo"
-    >
-      <span title="demo">foobar</span>
-      <span>barfoo</span>
-</div>`,
-    }),
-    `<div title="demo"><span title="demo">foobar</span><span>barfoo</span></div>`,
-  );
-});
-
 Deno.test("basic element with a child with a title", async () => {
   assertEquals(
     await htmlispToHTML({
@@ -113,44 +75,5 @@ Deno.test("element with a class", async () => {
   assertEquals(
     await htmlispToHTML({ htmlInput: `<div class="bar">foo</div>` }),
     `<div class="bar">foo</div>`,
-  );
-});
-
-Deno.test("siblings only", async () => {
-  assertEquals(
-    await htmlispToHTML({
-      htmlInput: `<div class="bar">foo</div><div class="bar">foo</div>`,
-    }),
-    `<div class="bar">foo</div><div class="bar">foo</div>`,
-  );
-});
-
-Deno.test("siblings only with former children", async () => {
-  assertEquals(
-    await htmlispToHTML({
-      htmlInput:
-        `<div class="bar"><span>foo</span></div><div class="bar">foo</div>`,
-    }),
-    `<div class="bar"><span>foo</span></div><div class="bar">foo</div>`,
-  );
-});
-
-Deno.test("siblings only with latter children", async () => {
-  assertEquals(
-    await htmlispToHTML({
-      htmlInput:
-        `<div class="bar">foo</div><div class="bar"><span>foo</span></div>`,
-    }),
-    `<div class="bar">foo</div><div class="bar"><span>foo</span></div>`,
-  );
-});
-
-Deno.test("siblings only with children", async () => {
-  assertEquals(
-    await htmlispToHTML({
-      htmlInput:
-        `<div class="bar"><span>foo</span></div><div class="bar"><span>foo</span></div>`,
-    }),
-    `<div class="bar"><span>foo</span></div><div class="bar"><span>foo</span></div>`,
   );
 });
