ede65af458b962708a3bfd8081789d01f65aeb0e
Juho Vepsäläinen <bebraw@gmail.com>
2024-03-21 11:36:14 +0200

    chore: Add a todo

--- htmlisp/utilities/parsers/parseTag_test.ts
+++ htmlisp/utilities/parsers/parseTag_test.ts
@@ -2,6 +2,8 @@ import { assertEquals } from "https://deno.land/std@0.142.0/testing/asserts.ts";
 import { parseTag } from "./parseTag.ts";
 import { asGenerator } from "./utils.ts";
 
+// TODO: Probably this should be able to parse pure content without tags as well
+
 Deno.test("self-closing parse tag", () => {
   assertEquals(
     parseTag(asGenerator(`<a href="test" title="foobar" />`)()),
