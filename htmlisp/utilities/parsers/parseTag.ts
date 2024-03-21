8f8cd83a6b295ca8466079f6dd059e3ca8353ec0
Juho Vepsäläinen <bebraw@gmail.com>
2024-03-21 11:29:04 +0200

    chore: Sketch out implementation

--- htmlisp/utilities/parsers/parseTag.ts
+++ htmlisp/utilities/parsers/parseTag.ts
@@ -1,30 +1,69 @@
-import * as states from "./states.ts";
 import { parseAttribute } from "./parseAttribute.ts";
 import type { Attributes, CharacterGenerator } from "./types.ts";
+
+const STATES = {
+  PARSE_TAG_START: "parse tag start",
+  PARSE_TAG_END: "parse tag end",
+  PARSE_TAG_NAME: "parse tag name",
+  PARSE_CHILDREN: "parse children",
+};
 
 // TODO: Use the same type here as for the earlier parser
 type Tag = {
   type: string;
   attributes?: Attributes;
   children?: (string | Tag)[];
+  closesWith?: string;
 };
 
 // TODO: After parsing an attribute, rollback and check the character here
 // TODO: To rewind, pass { rewind: true } to next() as a parameter
-function parseTag(
-  getCharacter: CharacterGenerator,
-): Tag {
-  const c = getCharacter.next();
+function parseTag(getCharacter: CharacterGenerator): Tag {
+  let state = STATES.PARSE_TAG_START;
+  let tag: Tag;
 
-  // TODO: The logic here should be able to parse a tag and use
-  // parseAttribute as a helper
-  console.log(c);
+  while (true) {
+    if (state === STATES.PARSE_TAG_START) {
+      // TODO
+      break;
+
+      tag.type = parseTagStart(getCharacter);
+      state = STATES.PARSE_TAG_NAME;
+    } else if (state === STATES.PARSE_TAG_NAME) {
+      // TODO
+      break;
+
+      const attribute = parseAttribute(getCharacter);
+
+      // TODO: Take a peek at the last character and figure out what to do next
+      const c = getCharacter.next({ previous: true });
+
+      if (c === " ") {
+        // Keep parsing attributes
+      } else if (c === ">") {
+        // Parse children
+      } else {
+        // Self-closing caes
+
+        tag.closesWith = c;
+
+        return tag;
+      }
+    } else if (state === STATES.PARSE_CHILDREN) {
+      // TODO
+      break;
+
+      // state = STATES.PARSE_TAG_END;
+    }
+  }
 
   return { type: "a", attributes: { href: "test", title: "foobar" } };
 }
 
 // TODO
-function* parseTagStart(value: string, c: string) {
+function parseTagStart(getCharacter: CharacterGenerator) {
+  return "foobar";
+
   /*
   if (c === ">") {
     parsingState = PARSE_CHILDREN_START;
@@ -44,11 +83,13 @@ function* parseTagStart(value: string, c: string) {
   }
   */
 
-  yield { value, state: states.IDLE };
+  // yield { value, state: states.IDLE };
 }
 
 // TODO
-function* parseTagEnd(value: string, c: string) {
+function parseTagEnd(getCharacter: CharacterGenerator) {
+  return "foobar";
+
   /*
   if (c === ">") {
     if (input[i - 1] !== "?") {
@@ -59,7 +100,7 @@ function* parseTagEnd(value: string, c: string) {
   }
   */
 
-  yield { value, state: states.IDLE };
+  // yield { value, state: states.IDLE };
 }
 
 export { parseTag, parseTagEnd, parseTagStart };
