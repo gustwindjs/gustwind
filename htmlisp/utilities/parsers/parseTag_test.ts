e23825be631e8d782fcefd6bf8ff0a86224afa88
Juho Vepsäläinen <bebraw@gmail.com>
2024-03-21 11:17:24 +0200

    chore: Sketch out specification

--- htmlisp/utilities/parsers/parseTag_test.ts
+++ htmlisp/utilities/parsers/parseTag_test.ts
@@ -2,9 +2,45 @@ import { assertEquals } from "https://deno.land/std@0.142.0/testing/asserts.ts";
 import { parseTag } from "./parseTag.ts";
 import { asGenerator } from "./utils.ts";
 
-Deno.test("parse tag", () => {
+Deno.test("self-closing parse tag", () => {
   assertEquals(
-    parseTag(asGenerator(`<a href="test" title="foobar">`)()),
+    parseTag(asGenerator(`<a href="test" title="foobar" />`)()),
     { type: "a", attributes: { href: "test", title: "foobar" } },
   );
 });
+
+Deno.test("parse tag", () => {
+  assertEquals(
+    parseTag(asGenerator(`<a href="test" title="foobar"></a>`)()),
+    { type: "a", attributes: { href: "test", title: "foobar" } },
+  );
+});
+
+Deno.test("parse tag with content", () => {
+  assertEquals(
+    parseTag(asGenerator(`<a href="test" title="foobar">barfoo</a>`)()),
+    {
+      type: "a",
+      attributes: { href: "test", title: "foobar" },
+      children: ["foobar"],
+    },
+  );
+});
+
+Deno.test("parse tag with another tag", () => {
+  assertEquals(
+    parseTag(
+      asGenerator(`<a href="test" title="foobar"><span>barfoo</span></a>`)(),
+    ),
+    {
+      type: "a",
+      attributes: { href: "test", title: "foobar" },
+      children: [{
+        type: "span",
+        children: ["barfoo"],
+      }],
+    },
+  );
+});
+
+// TODO: Siblings, closesWith cases
