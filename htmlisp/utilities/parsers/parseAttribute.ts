1897683b77d0fa3daf74f66f686742018bfc1156
Juho Vepsäläinen <bebraw@gmail.com>
2024-03-21 10:04:31 +0200

    chore: Sketch out parsing logic

--- htmlisp/utilities/parsers/parseAttribute.ts
+++ htmlisp/utilities/parsers/parseAttribute.ts
@@ -1,21 +1,40 @@
-import * as states from "./states.ts";
 import type { Attributes, CharacterGenerator } from "./types.ts";
+
+const STATES = {
+  PARSE_ATTRIBUTE_NAME: "parse attribute name",
+  PARSE_ATTRIBUTE_VALUE: "parse attribute value",
+  CAPTURE_ATTRIBUTE: "capture attribute",
+};
 
 function parseAttribute(
   getCharacter: CharacterGenerator,
 ): Attributes {
-  const c = getCharacter.next();
+  const { value } = getCharacter.next();
+  let state = STATES.PARSE_ATTRIBUTE_NAME;
 
   // TODO: The logic here should be able to parse a single attribute and
   // return it as an object
-  console.log(c);
+  console.log(value);
+
+  // 1. Parse attribute name
+  if (value === "=") {
+  } // Self-closing case
+  else if (value === "/") {
+    // TODO: Rewind and return
+  } else if (value === " ") {
+    // Ok,done
+  }
+
+  // If parsing name ended with =, parse value as well
+  // 2. Parse attribute value
+  // 3. Repeat the process until all attributes are clear
 
   return { href: "test" };
 }
 
 function* parseAttributeName(value: string, c: string) {
   if (c === "=") {
-    yield { value, mode: states.PARSE_ATTRIBUTE_VALUE };
+    yield { value, mode: STATES.PARSE_ATTRIBUTE_VALUE };
   } // Attribute name was not found after all
   else if (c === ">") {
     // Note that self-closing attributes can be found at the end of a tag
@@ -29,7 +48,7 @@ function* parseAttributeName(value: string, c: string) {
 
     parsingState = PARSE_CHILDREN_START;
     */
-    yield { value, state: states.CAPTURE_ATTRIBUTE };
+    yield { value, state: STATES.CAPTURE_ATTRIBUTE };
   } else if (c === "?" || c === "/") {
     // TODO: Figure out how to model this case -> push to another parser?
     // currentTag.closesWith = c;
@@ -47,9 +66,9 @@ function* parseAttributeName(value: string, c: string) {
 
     // TODO: Capture attribute name now, no value in this case
     // yield { value, mode: PARSE_ATTRIBUTE_NAME };
-    yield { value, state: states.CAPTURE_ATTRIBUTE };
+    yield { value, state: STATES.CAPTURE_ATTRIBUTE };
   } else {
-    yield { value: value + c, state: states.PARSE_ATTRIBUTE_NAME };
+    yield { value: value + c, state: STATES.PARSE_ATTRIBUTE_NAME };
   }
 }
 
@@ -60,7 +79,7 @@ function* parseAttributeValue(value: string, c: string) {
   if (c === '"') {
     if (singleQuotesFound > 0) {
       // Escape "'s in single quote mode
-      yield { value: value + `\"`, state: states.PARSE_ATTRIBUTE_VALUE };
+      yield { value: value + `\"`, state: STATES.PARSE_ATTRIBUTE_VALUE };
     } else {
       doubleQuotesFound++;
 
@@ -68,7 +87,7 @@ function* parseAttributeValue(value: string, c: string) {
         doubleQuotesFound = 0;
         singleQuotesFound = 0;
 
-        yield { value, state: states.PARSE_ATTRIBUTE_NAME };
+        yield { value, state: STATES.PARSE_ATTRIBUTE_NAME };
       }
     }
   } else if (c === "'") {
@@ -79,13 +98,13 @@ function* parseAttributeValue(value: string, c: string) {
         doubleQuotesFound = 0;
         singleQuotesFound = 0;
 
-        yield { value, state: states.PARSE_ATTRIBUTE_VALUE };
+        yield { value, state: STATES.PARSE_ATTRIBUTE_VALUE };
       }
     } else {
-      yield { value: value + c, state: states.PARSE_ATTRIBUTE_VALUE };
+      yield { value: value + c, state: STATES.PARSE_ATTRIBUTE_VALUE };
     }
   } else {
-    yield { value: value + c, state: states.PARSE_ATTRIBUTE_VALUE };
+    yield { value: value + c, state: STATES.PARSE_ATTRIBUTE_VALUE };
   }
 }
 
