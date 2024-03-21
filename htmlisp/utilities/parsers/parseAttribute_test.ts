fbcc1ff17c498c9fef71b9d2500e77b8b6462401
Juho Vepsäläinen <bebraw@gmail.com>
2024-03-21 10:04:31 +0200

    chore: Sketch out parsing logic

--- htmlisp/utilities/parsers/parseAttribute_test.ts
+++ htmlisp/utilities/parsers/parseAttribute_test.ts
@@ -11,8 +11,8 @@ Deno.test(`parse attribute with "`, () => {
 
 Deno.test(`parse attribute with '`, () => {
   assertEquals(
-    parseAttribute(asGenerator(`href='test' `)()),
-    { href: "test" },
+    parseAttribute(asGenerator(`href='foo' `)()),
+    { href: "foo" },
   );
 });
 
@@ -23,9 +23,26 @@ Deno.test(`parse attribute without a value`, () => {
   );
 });
 
+Deno.test(`parse attribute without a value at the end`, () => {
+  assertEquals(
+    parseAttribute(asGenerator(`href/`)()),
+    { href: null },
+  );
+});
+
+Deno.test(`parse attribute without a value at the end 2`, () => {
+  assertEquals(
+    parseAttribute(asGenerator(`href?`)()),
+    { href: null },
+  );
+});
+
+// TODO: In this case it should return and also rollback the generator a step somehow
+// Maybe it's a better idea to treat it as a closure instead since a method could be
+// added
 Deno.test(`parse attribute at the end`, () => {
   assertEquals(
-    parseAttribute(asGenerator(`href="test"/`)()),
-    { href: "test" },
+    parseAttribute(asGenerator(`href="foobar"/`)()),
+    { href: "foobar" },
   );
 });
