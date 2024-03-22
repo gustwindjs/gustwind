9b117b6ca95e0db53d174614b7ff74095e5e9dc6
Juho Vepsäläinen <bebraw@gmail.com>
2024-03-22 16:08:22 +0200

    chore: Add a test

--- htmlisp/utilities/parsers/parseAttributes_test.ts
+++ htmlisp/utilities/parsers/parseAttributes_test.ts
@@ -2,70 +2,77 @@ import { assertEquals } from "https://deno.land/std@0.142.0/testing/asserts.ts";
 import { parseAttributes } from "./parseAttributes.ts";
 import { asGenerator } from "./utils.ts";
 
-Deno.test(`parse attributes with "`, () => {
+Deno.test(`attributes with "`, () => {
   assertEquals(
     parseAttributes(asGenerator(`href="test" title="bar"`)()),
     { href: "test", title: "bar" },
   );
 });
 
-Deno.test(`parse attributes with " 2`, () => {
+Deno.test(`attributes with " 2`, () => {
   assertEquals(
     parseAttributes(asGenerator(`href="test" title="bar">`)()),
     { href: "test", title: "bar" },
   );
 });
 
-Deno.test(`parse attribute with '`, () => {
+Deno.test(`self-closing attribute at the end`, () => {
+  assertEquals(
+    parseAttributes(asGenerator(`href="test" title>`)()),
+    { href: "test", title: null },
+  );
+});
+
+Deno.test(`attribute with '`, () => {
   assertEquals(
     parseAttributes(asGenerator(`href='test' title='foo'`)()),
     { href: "test", title: "foo" },
   );
 });
 
-Deno.test(`parse attribute without a value`, () => {
+Deno.test(`attribute without a value`, () => {
   assertEquals(
     parseAttributes(asGenerator(`href title="foo"`)()),
     { href: null, title: "foo" },
   );
 });
 
-Deno.test(`parse attribute without a value at the end`, () => {
+Deno.test(`attribute without a value at the end`, () => {
   assertEquals(
     parseAttributes(asGenerator(`bar href/`)()),
     { bar: null, href: null },
   );
 });
 
-Deno.test(`parse attribute without a value at the end 2`, () => {
+Deno.test(`attribute without a value at the end 2`, () => {
   assertEquals(
     parseAttributes(asGenerator(`href?`)()),
     { href: null },
   );
 });
 
-Deno.test(`parse attribute at the end of a self-closing tag`, () => {
+Deno.test(`attribute at the end of a self-closing tag`, () => {
   assertEquals(
     parseAttributes(asGenerator(`href="foobar"/`)()),
     { href: "foobar" },
   );
 });
 
-Deno.test(`parse attribute at the end of a tag`, () => {
+Deno.test(`attribute at the end of a tag`, () => {
   assertEquals(
     parseAttributes(asGenerator(`href="foobar">`)()),
     { href: "foobar" },
   );
 });
 
-Deno.test(`parse tag end`, () => {
+Deno.test(`tag end`, () => {
   assertEquals(
     parseAttributes(asGenerator(` />`)()),
     {},
   );
 });
 
-Deno.test(`parse tag end 2`, () => {
+Deno.test(`tag end 2`, () => {
   assertEquals(
     parseAttributes(asGenerator(`href="test" title="foobar"></a>`)()),
     { href: "test", title: "foobar" },
