e7484e25eca9318e5c13d8244e3137d2d61b5465
Juho Vepsäläinen <bebraw@gmail.com>
2024-03-25 09:19:35 +0200

    chore: Simplify test naming

--- htmlisp/utilities/parsers/parseAttribute_test.ts
+++ htmlisp/utilities/parsers/parseAttribute_test.ts
@@ -2,56 +2,56 @@ import { assertEquals } from "https://deno.land/std@0.142.0/testing/asserts.ts";
 import { parseAttribute } from "./parseAttribute.ts";
 import { asGenerator } from "./utils.ts";
 
-Deno.test(`parse attribute with "`, () => {
+Deno.test(`attribute with "`, () => {
   assertEquals(
     parseAttribute(asGenerator(`href="test" `)()),
     ["href", "test"],
   );
 });
 
-Deno.test(`parse attribute with '`, () => {
+Deno.test(`attribute with '`, () => {
   assertEquals(
     parseAttribute(asGenerator(`title='foo' `)()),
     ["title", "foo"],
   );
 });
 
-Deno.test(`parse attribute without a value`, () => {
+Deno.test(`attribute without a value`, () => {
   assertEquals(
     parseAttribute(asGenerator(`href `)()),
     ["href", null],
   );
 });
 
-Deno.test(`parse attribute without a value at the end`, () => {
+Deno.test(`attribute without a value at the end`, () => {
   assertEquals(
     parseAttribute(asGenerator(`href/`)()),
     ["href", null],
   );
 });
 
-Deno.test(`parse attribute without a value at the end 2`, () => {
+Deno.test(`attribute without a value at the end 2`, () => {
   assertEquals(
     parseAttribute(asGenerator(`href?`)()),
     ["href", null],
   );
 });
 
-Deno.test(`parse attribute at the end of a self-closing tag`, () => {
+Deno.test(`attribute at the end of a self-closing tag`, () => {
   assertEquals(
     parseAttribute(asGenerator(`href="foobar"/`)()),
     ["href", "foobar"],
   );
 });
 
-Deno.test(`parse attribute at the end of a tag`, () => {
+Deno.test(`attribute at the end of a tag`, () => {
   assertEquals(
     parseAttribute(asGenerator(`href="foobar">`)()),
     ["href", "foobar"],
   );
 });
 
-Deno.test(`parse tag end`, () => {
+Deno.test(`tag end`, () => {
   assertEquals(
     parseAttribute(asGenerator(` />`)()),
     ["", null],
