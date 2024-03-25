14d910dfb946123868297f81787c2a1751b24056
Juho Vepsäläinen <bebraw@gmail.com>
2024-03-25 11:53:21 +0200

    chore: Add more tests

--- htmlisp/html-tests/integration_test.ts
+++ htmlisp/html-tests/integration_test.ts
@@ -32,7 +32,7 @@ Deno.test("head integration", async () => {
   );
 });
 
-Deno.test("full integration", async () => {
+Deno.test("full integration 1", async () => {
   assertEquals(
     await htmlispToHTML({
       htmlInput: `<!DOCTYPE html>
@@ -73,5 +73,134 @@ Deno.test("full integration", async () => {
       },
     }),
     `<!DOCTYPE html><html lang="en"><head><link rel="icon" href="demo"></link><link rel="preload" href="demo" as="style"></link><meta property="og:image" content="foobar/demo"></meta></head><body><div>nav</div><aside></aside><main><div>foobar</div></main><div>footer</div></body></html>`,
+  );
+});
+
+Deno.test("full integration 2", async () => {
+  assertEquals(
+    await htmlispToHTML({
+      htmlInput: `<BaseLayout content="<div>foobar</div>" />`,
+      context: { meta: { language: "en", title: "demo" } },
+      components: {
+        BaseLayout: `<!DOCTYPE html>
+        <html &lang="(get context meta.language)">
+          <head>
+            <link
+              rel="icon"
+              href="demo"
+            />
+            <link
+              rel="preload"
+              href="demo"
+              as="style"
+            />
+            <meta
+              property="og:image"
+              &content="(concat
+                foobar/
+                (get context meta.title)
+              )"
+            />
+          </head>
+          <body>
+            <MainNavigation />
+            <aside &children="(render (get props aside))"></aside>
+            <main &children="(render (get props content))"></main>
+            <MainFooter />
+          </body>
+        </html>`,
+        MainNavigation: "<div>nav</div>",
+        MainFooter: "<div>footer</div>",
+      },
+    }),
+    `<!DOCTYPE html><html lang="en"><head><link rel="icon" href="demo"></link><link rel="preload" href="demo" as="style"></link><meta property="og:image" content="foobar/demo"></meta></head><body><div>nav</div><aside></aside><main><div>foobar</div></main><div>footer</div></body></html>`,
+  );
+});
+
+Deno.test("full integration 3", async () => {
+  assertEquals(
+    await htmlispToHTML({
+      htmlInput:
+        `<BaseLayout><slot name="content"><div>foobar</div></slot></BaseLayout>`,
+      context: { meta: { language: "en", title: "demo" } },
+      components: {
+        BaseLayout: `<!DOCTYPE html>
+        <html &lang="(get context meta.language)">
+          <head>
+            <link
+              rel="icon"
+              href="demo"
+            />
+            <link
+              rel="preload"
+              href="demo"
+              as="style"
+            />
+            <meta
+              property="og:image"
+              &content="(concat
+                foobar/
+                (get context meta.title)
+              )"
+            />
+          </head>
+          <body>
+            <MainNavigation />
+            <aside &children="(render (get props aside))"></aside>
+            <main &children="(render (get props content))"></main>
+            <MainFooter />
+          </body>
+        </html>`,
+        MainNavigation: "<div>nav</div>",
+        MainFooter: "<div>footer</div>",
+      },
+    }),
+    `<!DOCTYPE html><html lang="en"><head><link rel="icon" href="demo"></link><link rel="preload" href="demo" as="style"></link><meta property="og:image" content="foobar/demo"></meta></head><body><div>nav</div><aside></aside><main><div>foobar</div></main><div>footer</div></body></html>`,
+  );
+});
+
+Deno.test("full integration 4", async () => {
+  assertEquals(
+    await htmlispToHTML({
+      htmlInput: `<BaseLayout>
+          <slot name="content">
+            <div><span>foobar</span></div>
+            <div>barfoo</div>
+          </slot>
+        </BaseLayout>`,
+      context: { meta: { language: "en", title: "demo" } },
+      components: {
+        BaseLayout: `<!DOCTYPE html>
+        <html &lang="(get context meta.language)">
+          <head>
+            <link
+              rel="icon"
+              href="demo"
+            />
+            <link
+              rel="preload"
+              href="demo"
+              as="style"
+            />
+            <meta
+              property="og:image"
+              &content="(concat
+                foobar/
+                (get context meta.title)
+              )"
+            />
+          </head>
+          <body>
+            <MainNavigation />
+            <aside &children="(render (get props aside))"></aside>
+            <main &children="(render (get props content))"></main>
+            <MainFooter />
+          </body>
+        </html>`,
+        MainNavigation: "<div>nav</div>",
+        MainFooter: "<div>footer</div>",
+      },
+    }),
+    `<!DOCTYPE html><html lang="en"><head><link rel="icon" href="demo"></link><link rel="preload" href="demo" as="style"></link><meta property="og:image" content="foobar/demo"></meta></head><body><div>nav</div><aside></aside><main><div><span>foobar</span></div><div>barfoo</div></main><div>footer</div></body></html>`,
   );
 });
