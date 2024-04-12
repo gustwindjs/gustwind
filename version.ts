51d25fae80294cb7c48a0b70c508d7fddc5c1e72
Juho Vepsäläinen <bebraw@gmail.com>
2024-04-12 14:51:39 +0300

    v0.66.7

--- version.ts
+++ version.ts
@@ -1,6 +1,6 @@
 import { compilePlugins } from "./compilePluginScripts.ts";
 
-export const VERSION = "0.66.6";
+export const VERSION = "0.66.7";
 
 export async function prepublish(_version: string) {
   try {
