a9d249a6dd84522de909732b4fde739283d34c81
Juho Vepsäläinen <bebraw@gmail.com>
2024-04-13 09:36:47 +0300

    chore: Add a todo

--- site/scripts/templatingPlayground.ts
+++ site/scripts/templatingPlayground.ts
@@ -1,5 +1,6 @@
-import { install, tw } from "https://cdn.skypack.dev/@twind/core@1.1.1?min";
+import { install } from "https://cdn.skypack.dev/@twind/core@1.1.1?min";
 import presetTailwind from "https://esm.sh/@twind/preset-tailwind@1.1.1";
+// TODO: Figure out how to make this import browser-compatible - jsdelivr does something different than unpkg here
 import HighlightJS from "https://unpkg.com/@highlightjs/cdn-assets@11.9.0/es/core.min.js";
 import highlightXML from "https://cdn.jsdelivr.net/npm/highlight.js@11.9.0/es/languages/xml.js";
 import { htmlispToHTML } from "../../htmlisp/mod.ts";
