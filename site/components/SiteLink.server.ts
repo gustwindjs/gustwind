b8eb9e71b700c67499cf38e2963c0ed6b6828930
Juho Vepsäläinen <bebraw@gmail.com>
2024-04-22 15:23:45 +0300

    fix: Restore internal url checks

--- site/components/SiteLink.server.ts
+++ site/components/SiteLink.server.ts
@@ -15,16 +15,11 @@ const init: GlobalUtilities["init"] = function init({ matchRoute }) {
 
     const [urlRoot, anchor] = url.split("#");
 
-    return url;
-
-    // TODO: Skip internal validation for now
-    /*
     if (await matchRoute(urlRoot)) {
       return urlRoot === "/"
         ? url
         : `/${urlRoot}${anchor ? "#" + anchor : "/"}`;
     }
-    */
 
     throw new Error(
       `Failed to find matching url for "${url}"`,
