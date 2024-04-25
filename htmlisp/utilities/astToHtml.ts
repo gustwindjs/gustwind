4d631d24b6bea50eff780bba149c4917e2d19c5c
Juho Vepsäläinen <bebraw@gmail.com>
2024-04-25 09:46:03 +0300

    refactor: Extract renderElement

--- htmlisp/utilities/astToHtml.ts
+++ htmlisp/utilities/astToHtml.ts
@@ -161,33 +161,42 @@ async function astToHtml(
       }
     }
 
-    const parsedChildren = parsedAttributes.children;
-
-    if (type !== "noop" && !parsedChildren && typeof closesWith === "string") {
-      return `<${type}${getAttributeBindings(parsedAttributes)}${closesWith}>`;
-    }
-
-    // TODO: Should there be a more strict check against parsed children?
-    const content = isString(parsedChildren)
-      ? parsedChildren.concat(await renderedChildren)
-      : await renderedChildren;
-
-    if (type === "noop" && !parsedAttributes.type) {
-      return content;
-    }
-
-    const t = type === "noop" && parsedAttributes.type || type;
-
-    if (t) {
-      if (type === "noop") {
-        delete parsedAttributes.type;
-      }
-
-      return `<${t}${getAttributeBindings(parsedAttributes)}>${content}</${t}>`;
-    }
-
-    return content;
+    return renderElement(parsedAttributes, tag, await renderedChildren);
   }))).join("");
+}
+
+function renderElement(
+  parsedAttributes: Record<string, unknown> & { children?: string },
+  tag: Element,
+  renderedChildren: string,
+) {
+  const parsedChildren = parsedAttributes.children || "";
+  const { type, closesWith } = tag;
+
+  if (type !== "noop" && !parsedChildren && typeof closesWith === "string") {
+    return `<${type}${getAttributeBindings(parsedAttributes)}${closesWith}>`;
+  }
+
+  // TODO: Should there be a more strict check against parsed children?
+  const content = isString(parsedChildren)
+    ? parsedChildren.concat(renderedChildren)
+    : renderedChildren;
+
+  if (type === "noop" && !parsedAttributes.type) {
+    return content;
+  }
+
+  const t = type === "noop" && parsedAttributes.type || type;
+
+  if (t) {
+    if (type === "noop") {
+      delete parsedAttributes.type;
+    }
+
+    return `<${t}${getAttributeBindings(parsedAttributes)}>${content}</${t}>`;
+  }
+
+  return content;
 }
 
 export { astToHtml };
