1f8d9336da8e79eca7afe568b2318ceaa1404a16
Juho Vepsäläinen <bebraw@gmail.com>
2024-04-25 09:25:04 +0300

    feat: Add sync versions to applying utilities

--- htmlisp/utilities/applyUtility.ts
+++ htmlisp/utilities/applyUtility.ts
@@ -29,6 +29,33 @@ async function applyUtilities<
   );
 }
 
+function applyUtilitiesSync<
+  U extends Utility,
+  US extends Utilities,
+  C extends Context,
+>(
+  props: Record<string, U> | null,
+  utilities: US,
+  context: C,
+): Record<string, unknown> {
+  if (!props) {
+    return {};
+  }
+
+  return Object.fromEntries(
+    Object.entries(props).map((
+      [k, v],
+    ) => [
+      k,
+      typeof v === "string" ? v : applyUtilitySync<U, US, C>(
+        v,
+        utilities,
+        context,
+      ),
+    ]),
+  );
+}
+
 async function applyUtility<
   U extends Utility,
   US extends Utilities,
@@ -77,4 +104,49 @@ async function applyUtility<
   return foundUtility.apply(context, parameters);
 }
 
-export { applyUtilities, applyUtility };
+function applyUtilitySync<
+  U extends Utility,
+  US extends Utilities,
+  C extends Context,
+>(
+  value: U,
+  utilities: US,
+  context: C,
+): any {
+  if (!utilities) {
+    throw new Error("applyUtility - No utilities were provided");
+  }
+
+  if (!value) {
+    return;
+  }
+
+  if (typeof value.utility !== "string") {
+    return;
+  }
+
+  const foundUtility = utilities[value.utility];
+
+  if (!foundUtility) {
+    console.error({ utilities, value });
+    throw new Error("applyUtility - Matching utility was not found");
+  }
+
+  const parameters = Array.isArray(value.parameters)
+    ? value.parameters.map((p) => {
+      if (typeof p === "string") {
+        // Nothing to do
+      } else if (p.utility && p.parameters) {
+        return applyUtilitySync(p, utilities, context);
+      }
+
+      return p;
+    })
+    : [];
+
+  // @ts-expect-error This is fine for now.
+  // TODO: Figure out a nice way to resolve context mismatch
+  return foundUtility.apply(context, parameters);
+}
+
+export { applyUtilities, applyUtilitiesSync, applyUtility, applyUtilitySync };
--- htmlisp/utilities/parseExpressions.ts
+++ htmlisp/utilities/parseExpressions.ts
@@ -1,5 +1,5 @@
 import { parseExpression } from "./parseExpression.ts";
-import { applyUtility } from "./applyUtility.ts";
+import { applyUtility, applyUtilitySync } from "./applyUtility.ts";
 import type { Attributes, Context } from "../types.ts";
 import type { Utilities, Utility } from "../../types.ts";
 
@@ -51,4 +51,50 @@ async function parseExpressions(
   return Object.fromEntries(entries);
 }
 
-export { parseExpressions };
+function parseExpressionsSync(
+  attributes: Attributes | undefined,
+  context: Context,
+  utilities: Utilities,
+) {
+  if (!attributes) {
+    return {};
+  }
+
+  const entries = Object.entries(attributes).map(
+    ([name, value]) => {
+      // Skip commented attributes
+      if (name.startsWith("__")) {
+        return;
+      }
+
+      // Check bindings
+      if (name.startsWith("&") && value !== null) {
+        const parsedExpression = parseExpression(value.toString());
+
+        if (!parsedExpression) {
+          throw new Error(`Failed to parse ${value} for attribute ${name}!`);
+        }
+
+        return [
+          name.slice(1),
+          applyUtilitySync<
+            Utility,
+            Utilities,
+            Context
+          >(
+            parsedExpression,
+            utilities,
+            context,
+          ),
+        ];
+      }
+
+      return [name, value];
+    },
+  ).filter(Boolean);
+
+  // @ts-expect-error This is fine
+  return Object.fromEntries(entries);
+}
+
+export { parseExpressions, parseExpressionsSync };
