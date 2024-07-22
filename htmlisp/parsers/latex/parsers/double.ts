50551bc2eaff96698a19177ae7e6270b39aea2c4
Juho Vepsäläinen <bebraw@gmail.com>
2024-07-22 15:09:12 +0300

    feat: Support parsing double expressions

--- htmlisp/parsers/latex/parsers/double.ts
+++ htmlisp/parsers/latex/parsers/double.ts
@@ -5,17 +5,21 @@ import type { Element } from "../../../types.ts";
 enum STATES {
   IDLE,
   PARSE_EXPRESSION,
-  PARSE_EXPRESSION_CONTENT,
+  PARSE_EXPRESSION_FIRST,
+  PARSE_EXPRESSION_SECOND,
 }
 
 const LIMIT = 100000;
 
-// Parses \<expression>{<parameter1>}{<parameter2>} form
+// Parses \<expression>{<parameter 1>}{<parameter 2>} form
 function parseDouble(
   expressions: Record<string, Expression>,
   getCharacter: CharacterGenerator,
-): Element[] {
-  let output = "";
+): Element {
+  let state = STATES.IDLE;
+  let foundKey = "";
+  let foundFirst = "";
+  let stringBuffer = "";
 
   for (let i = 0; i < LIMIT; i++) {
     const c = getCharacter.next();
@@ -24,10 +28,44 @@ function parseDouble(
       break;
     }
 
-    output += c;
+    if (state === STATES.IDLE) {
+      if (c === "\\") {
+        stringBuffer = "";
+        state = STATES.PARSE_EXPRESSION;
+      } else {
+        stringBuffer += c;
+      }
+    } else if (state === STATES.PARSE_EXPRESSION) {
+      if (c === "{") {
+        if (expressions[stringBuffer]) {
+          foundKey = stringBuffer;
+          stringBuffer = "";
+          state = STATES.PARSE_EXPRESSION_FIRST;
+        } else {
+          throw new Error(`Unknown expression: ${stringBuffer}`);
+        }
+      } else {
+        stringBuffer += c;
+      }
+    } else if (state === STATES.PARSE_EXPRESSION_FIRST) {
+      if (c === "}") {
+        foundFirst = stringBuffer;
+      } else if (c === "{") {
+        stringBuffer = "";
+        state = STATES.PARSE_EXPRESSION_SECOND;
+      } else {
+        stringBuffer += c;
+      }
+    } else if (state === STATES.PARSE_EXPRESSION_SECOND) {
+      if (c === "}") {
+        return expressions[foundKey](foundFirst, stringBuffer);
+      } else {
+        stringBuffer += c;
+      }
+    }
   }
 
-  return [{ type: "p", attributes: {}, children: [output] }];
+  throw new Error("No matching expression was found");
 }
 
 export { parseDouble };
--- htmlisp/parsers/latex/parsers/double_test.ts
+++ htmlisp/parsers/latex/parsers/double_test.ts
@@ -2,11 +2,24 @@ import { assertEquals } from "https://deno.land/std@0.142.0/testing/asserts.ts";
 import { parseDouble } from "./double.ts";
 import { characterGenerator } from "../../characterGenerator.ts";
 
-Deno.test(`id expression`, () => {
+Deno.test(`simple expression`, () => {
   const input = "foobar";
+  const arg = "demo";
 
   assertEquals(
-    parseDouble({}, characterGenerator(input)),
-    [{ type: "p", attributes: {}, children: [input] }],
+    parseDouble(
+      {
+        id: (i, arg) => ({
+          type: "div",
+          attributes: { id: arg || null },
+          children: [i],
+        }),
+      },
+      characterGenerator(String.raw`\id{${input}}{${arg}}`),
+    ),
+    { type: "div", attributes: { id: arg }, children: [input] },
   );
 });
+
+// TODO: Test that the logic throws in case a matching expression was not found
+// TODO: Test that the logic throws in case an expression is incomplete
