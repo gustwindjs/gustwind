7a023c817f427c84ef324bbc2c39fec5093faf8b
Juho Vepsäläinen <bebraw@gmail.com>
2024-08-02 11:54:22 +0300

    refactor: Sketch out parse interface

--- htmlisp/parsers/latex/parse.ts
+++ htmlisp/parsers/latex/parse.ts
@@ -1,37 +1,49 @@
+import { getParseContent } from "./parsers/content.ts";
 import { getParseSingle } from "./parsers/single.ts";
 import { getParseDouble } from "./parsers/double.ts";
 import { getParseBlock } from "./parsers/block.ts";
-import { getParseContent } from "./parsers/content.ts";
-import { blocks, doubles, singles } from "./defaultExpressions.ts";
 import { runParsers } from "./parsers/runParsers.ts";
 import type { CharacterGenerator } from "../types.ts";
+import type { Element } from "../../types.ts";
+import { type SingleParser } from "./parsers/single.ts";
+import { type DoubleParser } from "./parsers/double.ts";
+import { type BlockParser } from "./parsers/block.ts";
 
 const LIMIT = 100000;
 
-function parse<ExpressionReturnType>(
+function parse(
   getCharacter: CharacterGenerator,
-): ExpressionReturnType[] {
-  // TODO: Pass parsers here as a parameter as that also solves typing
-  const singleParsers = getParseSingle<ExpressionReturnType>(singles);
-  const doubleParsers = getParseDouble<ExpressionReturnType>(doubles);
-  const parsers = [
+  singleExpressions: Record<string, SingleParser<Element>>,
+  doubleExpressions: Record<string, DoubleParser<Element>>,
+  blockExpressions: Record<string, BlockParser<Element, Element>>,
+): Element[] {
+  const singleParsers = getParseSingle(singleExpressions);
+  const doubleParsers = getParseDouble(doubleExpressions);
+  const blockParsers = getParseBlock(blockExpressions);
+  const allParsers = [
     singleParsers,
     doubleParsers,
-    getParseBlock<ExpressionReturnType>(blocks),
-    getParseContent<ExpressionReturnType>((children) => ({
-      type: "p",
-      attributes: {},
-      children,
-    }), [singleParsers, doubleParsers]),
+    blockParsers,
+    getParseContent<Element>(
+      (children) => ({
+        type: "p",
+        attributes: {},
+        children,
+      }),
+      [singleParsers, doubleParsers],
+    ),
   ];
-
   const ret = [];
 
   for (let i = 0; i < LIMIT; i++) {
-    const parseResult = runParsers<ExpressionReturnType>(getCharacter, parsers);
+    const parseResult = runParsers<Element>(
+      getCharacter,
+      // @ts-expect-error This is fine for now. TODO: Fix runParsers type
+      allParsers,
+    );
 
-    if (parseResult) {
-      ret.push(parseResult);
+    if (parseResult?.match) {
+      ret.push(parseResult.value);
     }
 
     const c = getCharacter.next();
@@ -41,7 +53,6 @@ function parse<ExpressionReturnType>(
     }
   }
 
-  // @ts-ignore Ignore for now - most likely there's a type mismatch
   return ret;
 }
 
