ea27de502be20fd1cce3b7b2c8b490348410cb33
Juho Vepsäläinen <bebraw@gmail.com>
2024-09-04 15:47:42 +0300

    feat: Handle ~\cite with a whitespace based replacement

--- htmlisp/parsers/latex/parsers/content.ts
+++ htmlisp/parsers/latex/parsers/content.ts
@@ -48,6 +48,8 @@ function getParseContent<ExpressionReturnType>(
         } else {
           break;
         }
+      } else if (c === "~" && getCharacter.get() === "\\") {
+        stringBuffer += " ";
       } else {
         stringBuffer += c;
       }
