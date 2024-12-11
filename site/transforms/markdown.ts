28f888d368df2e1b6b9673f9b472e7ae1c9a9a7b
Juho Vepsäläinen <bebraw@gmail.com>
2024-12-11 16:01:45 +0200

    chore: Update marked

--- site/transforms/markdown.ts
+++ site/transforms/markdown.ts
@@ -1,5 +1,6 @@
 import { install, tw } from "https://esm.sh/@twind/core@1.1.1";
-import { marked } from "https://cdn.jsdelivr.net/npm/marked@9.1.5/lib/marked.esm.js";
+import { marked } from "https://unpkg.com/marked@15.0.3/lib/marked.esm.js";
+import type { Renderer } from "https://unpkg.com/marked@15.0.3/lib/marked.d.ts";
 import type { DataSourcesApi } from "../../types.ts";
 import highlight from "https://cdn.jsdelivr.net/npm/@highlightjs/cdn-assets@11.10.0/es/core.min.js";
 import highlightBash from "https://cdn.jsdelivr.net/npm/highlight.js@11.10.0/es/languages/bash.js";
@@ -83,103 +84,120 @@ function getTransformMarkdown({ load, renderSync }: DataSourcesApi) {
       },
     });
 
-    // https://marked.js.org/using_pro#renderer
-    // https://github.com/markedjs/marked/blob/master/src/Renderer.js
-    marked.use({
-      renderer: {
-        code(code: string, lang: string): string {
+    const renderer: Pick<
+      Renderer,
+      "code" | "heading" | "link" | "list"
+    > = {
+      code({ text: code, lang }) {
+        // @ts-ignore How to type this?
+        if (this.options.highlight) {
+          // TODO: Inject highlight.js now through `load.style`
+          // This should replace BaseLayout bits that currently exist.
+          // Furthermore, injection should be unique.
+          // Likely style should be exposed as markdown parameter so it
+          // is easy to change.
+
           // @ts-ignore How to type this?
-          if (this.options.highlight) {
-            // TODO: Inject highlight.js now through `load.style`
-            // This should replace BaseLayout bits that currently exist.
-            // Furthermore, injection should be unique.
-            // Likely style should be exposed as markdown parameter so it
-            // is easy to change.
+          const out = this.options.highlight(code, lang);
 
-            // @ts-ignore How to type this?
-            const out = this.options.highlight(code, lang);
-
-            if (out != null && out !== code) {
-              code = out;
-            }
+          if (out != null && out !== code) {
+            code = out;
           }
+        }
 
-          code = code.replace(/\n$/, "") + "\n";
+        code = code.replace(/\n$/, "") + "\n";
 
-          if (!lang) {
-            return "<pre><code>" +
-              code +
-              "</code></pre>\n";
-          }
-
-          return '<pre class="' +
-            tw("overflow-auto -mx-4 md:mx-0") +
-            '"><code class="' +
-            // @ts-ignore How to type this?
-            (this.options.langPrefix || "") +
-            lang +
-            '">' +
+        if (!lang) {
+          return "<pre><code>" +
             code +
             "</code></pre>\n";
-        },
-        heading(
-          text: string,
-          level: number,
-          raw: string,
-        ) {
-          const slug = slugify(raw);
+        }
 
-          tableOfContents.push({ slug, level, text });
-
-          return "<h" +
-            level +
-            ' id="' +
-            slug +
-            '">' +
-            text +
-            '<a class="' +
-            tw("ml-2 no-underline text-sm align-middle mask-text") +
-            '" href="#' +
-            slug +
-            '">🔗</a>\n' +
-            "</h" +
-            level +
-            ">\n";
-        },
-        link(href: string, title: string, text: string) {
-          if (href === null) {
-            return text;
-          }
-
-          if (text === "<file>") {
-            // TODO: Show a nice error in case href is not found in the fs
-            const fileContents = load.textFileSync(href);
-
-            return this.code(fileContents, href.split(".").at(-1) as string);
-          }
-
-          let out = '<a class="' + tw("underline") + '" href="' + href + '"';
-          if (title) {
-            out += ' title="' + title + '"';
-          }
-          out += ">" + text + "</a>";
-          return out;
-        },
-        list(body: string, ordered: string, start: number) {
-          const type = ordered ? "ol" : "ul",
-            startatt = (ordered && start !== 1)
-              ? (' start="' + start + '"')
-              : "",
-            klass = ordered
-              ? "list-decimal list-inside"
-              : "list-disc list-inside";
-          return "<" + type + startatt + ' class="' + tw(klass) + '">\n' +
-            body +
-            "</" +
-            type + ">\n";
-        },
+        return '<pre class="' +
+          tw("overflow-auto -mx-4 md:mx-0") +
+          '"><code class="' +
+          // @ts-ignore How to type this?
+          (this.options.langPrefix || "") +
+          lang +
+          '">' +
+          code +
+          "</code></pre>\n";
       },
-    });
+      heading(token) {
+        // @ts-expect-error Parser will exist
+        const text = this.parser.parseInline(token.tokens);
+        const level = token.depth;
+        const slug = slugify(token.raw);
+
+        tableOfContents.push({ slug, level, text });
+
+        return "<h" +
+          level +
+          ' id="' +
+          slug +
+          '">' +
+          text +
+          '<a class="' +
+          tw("ml-2 no-underline text-sm align-middle mask-text") +
+          '" href="#' +
+          slug +
+          '">🔗</a>\n' +
+          "</h" +
+          level +
+          ">\n";
+      },
+      link({ href, title, tokens }) {
+        // @ts-expect-error Parser will exist
+        const text = this.parser.parseInline(tokens);
+
+        if (href === null) {
+          return text;
+        }
+
+        if (text === "<file>") {
+          // TODO: Show a nice error in case href is not found in the fs
+          const fileContents = load.textFileSync(href);
+
+          return this.code({
+            type: "code",
+            text: fileContents,
+            lang: href.split(".").at(-1) as string,
+            raw: fileContents,
+          });
+        }
+
+        let out = '<a class="' + tw("underline") + '" href="' + href + '"';
+        if (title) {
+          out += ' title="' + title + '"';
+        }
+        out += ">" + text + "</a>";
+        return out;
+      },
+      list({ ordered, start, items }) {
+        // Copied from marked source
+        let body = "";
+        for (let j = 0; j < items.length; j++) {
+          const item = items[j];
+
+          // @ts-expect-error Use default listitem
+          body += this.listitem(item);
+        }
+
+        const type = ordered ? "ol" : "ul",
+          startatt = (ordered && start !== 1) ? (' start="' + start + '"') : "",
+          klass = ordered
+            ? "list-decimal list-inside"
+            : "list-disc list-inside";
+        return "<" + type + startatt + ' class="' + tw(klass) + '">\n' +
+          body +
+          "</" +
+          type + ">\n";
+      },
+    };
+
+    // https://marked.js.org/using_pro#renderer
+    // https://github.com/markedjs/marked/blob/master/src/Renderer.js
+    marked.use({ renderer });
 
     return { content: await marked(input), tableOfContents };
   };
